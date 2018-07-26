0723a8ee47a2a763be625d2e3fab93525b9a86d6
Towhid Zaman <towhid1zaman@gmail.com>
2018-07-27 03:37:00 +0600

    Add solution for problem `A - Calculating Function` Link: https://codeforces.com/contest/486/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/486/submission/40819263

---
+++ codeforces/486/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n,l,m;
+            cin>>n;
+        if(n%2==0){
+                  l = n/2;
+        cout<<l<<endl;
+        }
+    else
+        {
+             m = -((n+1)/2);
+            cout<<m<<endl;
+        }
+
+        return 0;
+}
