19fb05ac468e1681146221706214e14251ac7a37
Towhid Zaman <towhid1zaman@gmail.com>
2018-08-04 04:31:00 +0600

    Add solution for problem `A - Death Note` Link: https://codeforces.com/contest/1016/problem/A Tags: greedy, implementation, math, *900 Ref: https://codeforces.com/contest/1016/submission/41194437

--- codeforces/1016/A.cpp
+++ codeforces/1016/A.cpp
@@ -1,17 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    int n,m;
-    cin >> n >> m;
-    int result = 0;
-    vector<int> vect(n);
-    for(int i=0;i<n;i++)
-    {
-        cin >> vect[i];
-        result += vect[i];
-        cout << result/m <<" ";
-        result %= m;
-    }
+	long long int sum=0,dif,i,n,m;
+	cin>>n;
+	cin>>m;
+	long long int a[n];
+	long long int t[n];
+
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+		t[i]=0;
+	}
+
+
+	for(i=0;i<n;i++)
+	{
+		if(sum<m)
+		{
+			sum+=a[i];
+		}
+
+		if (sum>=m)
+		{
+		  t[i]=sum/m;
+			sum=sum%m;
+		}
+	}
+
+	for (i=0;i<n;i++)
+        cout << t[i] << " ";
     return 0;
+
 }
