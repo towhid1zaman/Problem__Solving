b68efe61cb59ce1d0f4d0487320de177dd46dd95
Towhid Zaman <towhid1zaman@gmail.com>
2018-08-21 21:31:00 +0600

    Add solution for problem `A - IQ test` Link: https://codeforces.com/contest/25/problem/A Tags: brute force, *1300 Ref: https://codeforces.com/contest/25/submission/41941109

--- codeforces/25/A.cpp
+++ codeforces/25/A.cpp
@@ -7,24 +7,30 @@ const double pi= acos(-1);
 int main()
 {
     ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	ll m,t;
-	ll b,c,x=0,y=0;
-	cin>>t;
-	f1(t){
-        cin>>m;
-            if(m&1){
-                x++;b=i;
-            }
-            else{
-                y++;c=i;
-            }
-        }
-	if(x>y)
-        cout<<c<<endl;
-	else
-        cout<<b<<endl;
+    cin.tie(0);
+    cout.tie(0);
 
+    ll n,x;
+    cin>>n;
+    ll O[n],o=0;
+    ll E[n],e=0;
+    f0(n)
+    {
+        cin>>O[i];
+    }
+
+    f0(n)
+    {
+        if(O[i]&1) o++;
+    }
+    if(o==1)
+    {
+        f0(n)if(O[i]&1) cout<<i+1<<endl;
+    }
+
+    else
+    {
+        f0(n)if((O[i]&1)==0) cout<<i+1<<endl;
+    }
     return 0;
 }
