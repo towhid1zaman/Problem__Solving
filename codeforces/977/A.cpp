54ee996a90b46e2be6ad20538932e28fe6e8d017
Towhid Zaman <towhid1zaman@gmail.com>
2018-12-02 04:09:00 +0600

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/46463022

--- codeforces/977/A.cpp
+++ codeforces/977/A.cpp
@@ -1,59 +1,38 @@
-
-// </> : towhid1zaman
-
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair< int, int > pii;
-typedef vector<pii>vip;
-typedef pair< pii, int > ppi;
-#define fill(a,b) memset(a,b,sizeof(a))
-#define all(v) (v).begin(),(v).end()
-#define sp(k) cout<<setprecision(k)<<fixed;
-#define rep(i,a) for(int i=0;i<a;i++)
-#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
-#define irep(i,b,a) for(int i=(b);i>=(a);--i)
-#define minv(v) *min_element(all(v))
-#define maxv(v) *max_element(all(v))
-#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
-#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl "\n"
-#define sqr(a) ((a)*(a))
-#define sz(a) int(a.size())
-#define ff first
-#define ss second
-#define pb push_back
 
-const double pi = acos(-1.0);
-const int mod = 1000000007; // (int)1e9+7
-const int inf = 0x3f3f3f3f;// (int)3e18;
-const int maxn = 200005;
- 
-void task(){
-    ll n,k;cin >> n >> k;
-    while(k--){
-        ll last = n%10;
-        if(last==0)n/=10;
-        else n-=1;
-    }
-    cout << n << endl;
-}
+#define         ll long long
+#define         f0(n) for(size_t i=0;i<n;i++)
+#define         f1(n) for(size_t i=1;i<=n;i++)
+#define         mem(a) memset(a,0,sizeof(a))
+#define         memn(a) memset(a,-1,sizeof(a))
+#define         pb push_back
+#define         nl '\n'
 
-int main(){
-        _
-        #ifdef OJ
-        freopen("inputf.in", "r", stdin);
-        freopen("outputf.in", "w", stdout);
-        #endif
-
-        int T = 1; //cin >> T;
-        while(T--){
-            task();
+int in() {int n;scanf("%d", &n);return n;}
+ll Lin() {ll n;scanf("%lld", &n);return n;}
+double Din() {double n;scanf("%lf", &n);return n;}
+//long long lcm = (x*y)/(__gcd(x,y));
+const double pi= acos(-1);
+#define NFS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+int main()
+{
+	NFS
+    ll n = Lin(), k = Lin();
+        for (ll i = 1; i <= k; i++) {
+		if (n % 10 != 0)
+        {
+            n--;
         }
-        
-return 0;
-}
+		else
+        {
+            n /= 10;
+        }
+	}
+	cout << n;
 
+
+
+    return 0;
+}
 
