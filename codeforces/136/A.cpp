0493db9f55c09f9f6fbacd099302bfa57a7b0af9
Towhid Zaman <towhid1zaman@gmail.com>
2018-12-17 02:04:00 +0600

    Add solution for problem `A - Presents` Link: https://codeforces.com/contest/136/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/136/submission/47141016

--- codeforces/136/A.cpp
+++ codeforces/136/A.cpp
@@ -3,23 +3,19 @@ using namespace std;
 int main()
 {
     int n;
-      cin>>n;
-        int an[n];
-        int bn[n];
+    int an[101];
+    cin>>n;
 
-                    for(int i = 0; i<n; i++)
-                    {
-                        cin>>an[i];
-                    }
-                        for(int i = 0; i<n; i++)
-                        {
-                            bn[an[i] - 1] = i+1;
-                        }
-                        for(int i = 0;i<n;i++)
-                        {
-                            cout<<bn[i]<<" ";
-                        }
-                        cout<<endl;
+            int k;
+            for(int i = 1; i<=n;i++)
+            {
+                cin>>k;
+                    an[k] = i;
+            }
+                for(int i = 1; i<=n; i++)
+                {
+                    cout<<an[i] <<" ";
+                }
+                return 0;
 
-        return 0;
 }
