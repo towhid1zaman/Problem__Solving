e92adc891d021c18a861a8c9c41de440bbb4c4e9
Towhid Zaman <towhid1zaman@gmail.com>
2019-01-02 15:53:00 +0600

    Add solution for problem `A - Definite Game` Link: https://codeforces.com/contest/1081/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1081/submission/47835384

---
+++ codeforces/1081/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+int main()
+{
+    LL t;
+            cin>>t;
+            if(t==2)cout<<"2"<<endl;
+            else cout<<"1"<<endl;
+    return 0;
+}
+
