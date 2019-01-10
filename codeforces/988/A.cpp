386195b0c3564efe673febb2dc88ca8e09afcd5e
Towhid Zaman <towhid1zaman@gmail.com>
2019-01-11 00:51:00 +0600

    Add solution for problem `A - Diverse Team` Link: https://codeforces.com/contest/988/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/988/submission/48192586

--- codeforces/988/A.cpp
+++ codeforces/988/A.cpp
@@ -1,62 +1,39 @@
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair <int, int> pii;
-typedef vector<pii>vip;
-#define all(v) (v).begin(),(v).end()
-#define sp(k) cout<<setprecision(k)<<fixed;
-#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
-#define minv(v) *min_element(v.begin(),v.end())
-#define maxv(v) *max_element(v.begin(),v.end())
-#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
-#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
-#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
-#define endl "\n"
-#define F first
-#define S second
-#define pb push_back
-#define mp make_pair
-const int mod = 1000000007; // (int)1e9+7
-const int N = 200100;
-int pos[N];
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cerr.tie(nullptr);
-    int n,k;
-    cin>>n>>k;
-    int p;
-    set<int>s;
-    //multiset<int>ms;
-
-    for(int i = 1; i<=n;i++){
-        cin>>p;
-        pos[p] = i;
-        s.insert(p);
-
-    }
-    if(s.size()<k){
-            cout<<"NO"<<endl;
-            return 0;
-    }
-    else
-        cout<<"YES"<<endl;
-        int cnt = 0;
-        for(auto z: s){
-                if(cnt==k)break;
-                cout<<pos[z]<<" ";
-            cnt++;
-        }
-        cout<<endl;
+typedef long long LL;
+int a[101];
+int main()
+{
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 
+        int n,k;
+        cin>>n>>k;
+         map<int,int>mp;
+         vector<int>v;
+            for(int i = 0; i<n;i++)
+            {
+                int x;
+                cin>>x;
+                    if(mp[x]==0)
+                    {
+                          v.push_back(i+1);
+                           mp[x] = 1;
+                    }
 
-return 0;
+            }
+                if(v.size()<k) cout<<"NO"<<endl;
+                else
+                {
+                    cout<<"YES"<<endl;
+                        for(int i = 0; i<k;i++)
+                        {
+                            cout<<v[i]<<" ";
+                        }
+                        cout<<endl;
+                }
+
+
+        return 0;
 }
-
-
-
 
