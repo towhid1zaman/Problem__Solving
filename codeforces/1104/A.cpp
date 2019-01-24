af3277166be181d40ad540049daca5f871696721
Towhid Zaman <towhid1zaman@gmail.com>
2019-01-24 11:56:00 +0600

    Add solution for problem `A - Splitting into digits` Link: https://codeforces.com/contest/1104/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/1104/submission/48870956

---
+++ codeforces/1104/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int main()
+{
+  	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+        int n;
+        cin>>n;
+        if(n>0)
+        {
+            cout<<n<<endl;
+        }
+        for(int i = 0; i<n;i++)
+            cout<<"1"<<" ";
+    return 0;
+}
+
