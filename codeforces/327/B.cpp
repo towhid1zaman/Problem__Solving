ba563253c617e0dfed8596bf7d6f15b51156b29e
Towhid Zaman <towhid1zaman@gmail.com>
2019-02-16 21:30:00 +0600

    Add solution for problem `B - Hungry Sequence` Link: https://codeforces.com/contest/327/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/327/submission/49997144

--- codeforces/327/B.cpp
+++ codeforces/327/B.cpp
@@ -6,8 +6,8 @@ int main()
         ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
        int n;
        cin>>n;
-       for(int i = 0; i<n;i++){
-        cout<<i+1000000<<" ";
+       for(int i = n; i<2*n;i++){
+        cout<<i<<" ";
        }
        cout<<endl;
 
