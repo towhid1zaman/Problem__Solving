377ccc1d9c0455a193a10750600dca798587c292
Towhid Zaman <towhid1zaman@gmail.com>
2019-03-02 01:31:00 +0600

    Add solution for problem `A - Vanya and Table` Link: https://codeforces.com/contest/552/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/552/submission/50655853

--- codeforces/552/A.cpp
+++ codeforces/552/A.cpp
@@ -2,36 +2,20 @@
 using namespace std;
 
 typedef long long LL;
- int table[100][100];
+
 int main()
 {
 
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         int n;
         cin>>n;
-
-          int x1,y1,x2,y2;
-            while(n--){
-                cin>>x1>>y1>>x2>>y2;
-                x1--;
-                x2--;
-                y1--;
-                y2--;
-                for(int i = x1; i<=x2;i++){
-                    for(int j = y1; j<=y2;j++){
-                        table[i][j] ++;
-                    }
-                }
-
-            } int res = 0;
-                    for(int i = 0; i<100;i++){
-                        for(int j = 0; j<100;j++){
-                            res+=table[i][j];
-                        }
-                    }
-                    cout<<res<<endl;
-
-
+        int res = 0;
+        for(int i = 0; i<n;i++){
+            int x1,y1,x2,y2;
+            cin>>x1>>y1>>x2>>y2;
+            res+=(x2-x1+1)*(y2-y1+1);
+        }
+        cout<<res<<endl;
 
 
     return 0;
