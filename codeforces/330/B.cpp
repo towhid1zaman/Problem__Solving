33854ebd4ba8b507de4ba33a5f6f501378967c7d
Towhid Zaman <towhid1zaman@gmail.com>
2019-03-21 15:57:00 +0600

    Add solution for problem `B - Road Construction` Link: https://codeforces.com/contest/330/problem/B Tags: constructive algorithms, graphs, *1300 Ref: https://codeforces.com/contest/330/submission/51610322

---
+++ codeforces/330/B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int v[1000005];
+int main()
+{
+     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+            int n,m;
+            cin>>n>>m;
+            //vector<int>v;
+            memset(v,0,sizeof(v));
+            for(int i  = 0; i<m;i++){
+                int x,y;
+                cin>>x>>y;
+                v[x] = 1;
+                v[y] = 1;
+            }
+            int star;
+            for(int i = 1; i<=n;i++){
+                if( !v[i]){
+                    star = i;
+                    break;
+                }
+            }
+            cout<<n-1<<endl;
+            for(int i = 1; i<=n;i++)
+            {
+                if(star==i)continue;
+                    cout<<star<<" "<<i<<endl;
+            }
+
+     return 0;
+}
