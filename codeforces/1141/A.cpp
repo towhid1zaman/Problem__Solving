271aee12e600f010be9688cf4c8763237a1ff176
Towhid Zaman <towhid1zaman@gmail.com>
2019-03-22 12:27:00 +0600

    Add solution for problem `A - Game 23` Link: https://codeforces.com/contest/1141/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1141/submission/51666781

--- codeforces/1141/A.cpp
+++ codeforces/1141/A.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-#define 	RL(x) 		scanf("%lld",&x)
-#define 	R(x) 		scanf("%d",&x)
-#define 	pb     		push_back
+#define 	RL(x) 		        scanf("%lld",&x)
+#define 	R(x) 		            scanf("%d",&x)
+#define 	pb     		        push_back
 #define 	lcm(a,b)       	a * (b / gcd(a, b))
 #define 	gcd(a,b)       	__gcd(a,b)
-#define 	nl 		'\n'
+#define 	nl 		            '\n'
 int main()
 {
      ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -36,7 +36,6 @@ int main()
                 if(d != 1) res = -1;
         }
                 cout<<res<<nl;
-
      return 0;
 }
 
