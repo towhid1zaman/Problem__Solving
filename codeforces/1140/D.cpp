9684da9ab75e24194e8b1f9625b8eb90d95edd55
Towhid Zaman <towhid1zaman@gmail.com>
2019-03-23 00:15:00 +0600

    Add solution for problem `D - Minimum Triangulation` Link: https://codeforces.com/contest/1140/problem/D Tags: dp, greedy, math, *1200 Ref: https://codeforces.com/contest/1140/submission/51724109

---
+++ codeforces/1140/D.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+#define 	pb     	  push_back
+#define 	lcm(a,b)       a * (b / gcd(a, b))
+#define 	gcd(a,b)        __gcd(a,b)
+#define 	nl 	  "\n"
+#define 	inf 	  2e16
+#define 	pi	  acos(-1.0)
+#define 	ff 	  first
+#define 	ss 	  second
+#define 	mp 	  make_pair
+#define 	min3(a,b,c) min(a,min(b,c))
+#define 	max3(a,b,c) max(a,max(b,c))
+#define 	sqr(a) 	  ((a)*(a))
+#define 	mem(a,x) 	  memset(a,x,sizeof(a))
+#define 	RL(x) 	  scanf("%lld",&x)
+#define 	R(x) 	  scanf("%d",&x)
+#define 	SORT(v) 	  sort(v.begin(),v.end())
+#define 	REV(v) 	  reverse(v.begin(),v.end())
+#define	f(i,a,b)	  for(int i = a; i<=b;i++)
+int main()
+{
+     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+      LL n;
+      RL(n);
+      LL res = 0;
+      for(LL i = 2; i<n;i++)
+      {
+            res+=i*(i+1);
+      }
+      cout<<res<<endl;
+
+     return 0;
+}
+
+
+
