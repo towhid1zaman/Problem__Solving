720a7df54150b97c2112ea157c1bb83c61b54421
Towhid Zaman <towhid1zaman@gmail.com>
2019-03-23 16:13:00 +0600

    Add solution for problem `B - Different is Good` Link: https://codeforces.com/contest/672/problem/B Tags: constructive algorithms, implementation, strings, *1000 Ref: https://codeforces.com/contest/672/submission/51752713

---
+++ codeforces/672/B.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+#define pb push_back
+#define lcm(a,b) a * (b / gcd(a, b))
+#define gcd(a,b) __gcd(a,b)
+#define nl "\n"
+#define inf 2e16
+#define pi	acos(-1.0)
+#define ff first
+#define ss second
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define sqr(a) ((a)*(a))
+#define mem(a,x) memset(a,x,sizeof(a))
+#define RL(x) scanf("%lld",&x)
+#define R(x) scanf("%d",&x)
+#define RS(x) cin>>s
+//#define RSL(x) scanf(" %[^\n]", x)
+#define RSL(cin, x) getline(cin, x)
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define f(i,a,b) for(int i = a; i<=b;i++)
+int main()
+{
+     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+      int t;
+        cin>>t;
+      string s;
+      cin>>s;
+      int sz = s.length();
+      if(sz>26){
+        cout<<-1<<nl;
+        return 0;
+      }
+      set<char>st;
+      for(int i = 0; i<sz;i++)
+      {
+          st.insert(s[i]);
+      }
+      cout<<abs(t - st.size())<<nl;
+     return 0;
+}
+
+
+
