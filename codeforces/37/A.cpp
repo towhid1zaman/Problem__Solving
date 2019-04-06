22b8821c061a865569118a512bc8f859ca615418
Towhid Zaman <towhid1zaman@gmail.com>
2019-04-06 13:01:00 +0600

    Add solution for problem `A - Towers` Link: https://codeforces.com/contest/37/problem/A Tags: sortings, *1000 Ref: https://codeforces.com/contest/37/submission/52372911

---
+++ codeforces/37/A.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<double , double> pdd;
+typedef pair<LL , LL > pll;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<LL> vl;
+
+#define pb push_back
+#define lcm(a,b) a * (b / gcd(a, b))
+#define gcd(a,b) __gcd(a,b)
+#define nl "\n"
+#define inf 2e16
+#define MAX (int)2e5+5
+#define pi	acos(-1.0)
+#define ff first
+#define ss second
+#define mp make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define sqr(a) ((a)*(a))
+#define mem(a,x) memset(a,x,sizeof(a))
+#define RL(x) scanf("%lld",&x)
+#define R(x) scanf("%d",&x)
+#define RS(x) cin>>s
+//#define RSL(x) scanf(" %[^\n]", x)
+#define RSL(cin, x) getline(cin, x)
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define f(i,a,b) for(int i = a; i<=b;i++)
+#define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+int main()
+{
+    IOS
+
+//            10
+//            5 5 6 3 3 3 2 8 9 10
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        map<int, int>mp;
+        int mx = 0;
+        for(int i = 0; i<n;i++)
+        {
+            cin>>v[i];
+            mp[v[i] ] ++;
+            //cout<<"MP  "<<mp[v[i] ]<<nl;
+            if(mp[v[i]] > mx){
+                mx  = mp[v[i] ];
+            }
+           // cout<<"MX " <<mx<<nl;
+        }
+        cout<<mx<<" "<<mp.size()<<nl;
+
+
+
+
+
+     return 0;
+}
