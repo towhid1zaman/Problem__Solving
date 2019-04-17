c0dcecafb1dbd02b4a3888deae2ce853fcbcaf8c
Towhid Zaman <towhid1zaman@gmail.com>
2019-04-18 00:52:00 +0600

    Add solution for problem `B - Make Them Equal` Link: https://codeforces.com/contest/1154/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/1154/submission/52923107

--- codeforces/1154/B.cpp
+++ codeforces/1154/B.cpp
@@ -1,64 +1,74 @@
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<double , double> pdd;
+typedef pair<LL , LL > pll;
 typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair <int, int> pii;
-typedef vector<pii>vip;
-#define all(v) (v).begin(),(v).end()
-#define sp(k) cout<<setprecision(k)<<fixed;
-#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
-#define minv(v) *min_element(v.begin(),v.end())
-#define maxv(v) *max_element(v.begin(),v.end())
-#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
-#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
-#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
-#define endl "\n"
-#define F first
-#define S second
+typedef vector<pii> vpii;
+typedef vector<LL> vl;
+
 #define pb push_back
+#define lcm(a,b) a * (b / gcd(a, b))
+#define gcd(a,b) __gcd(a,b)
+#define nl "\n"
+#define inf 2e16
+#define MAX (int)2e5+5
+#define pi	acos(-1.0)
+#define ff first
+#define ss second
 #define mp make_pair
-const int mod = 1000000007; // (int)1e9+7
-const int N = 200100;
+#define check printf("OK\n");
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define sqr(a) ((a)*(a))
+#define mem(a,x) memset(a,x,sizeof(a))
+#define RL(x) scanf("%lld",&x)
+#define R(x) scanf("%d",&x)
+#define RS(x) cin>>s
+//#define RSL(x) scanf(" %[^\n]", x)
+#define RSL(cin, x) getline(cin, x)
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define f(i,a,b) for(int i = a; i<=b;i++)
+#define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+int main()
+{
+    IOS
+                int N;
+                cin>>N;
+                vector<int>V(N);
+                set<int>s;
+                for(int i = 0; i<N;i++){
+                    cin>>V[i];
+                    s.insert(V[i]);
+                }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cerr.tie(nullptr);
-    int n;
-    cin>>n;
-    int a[n+1];
-    for(int i = 0; i<n;i++){
-        cin>>a[i];
-    }
-    sort(a,a+n);
-    if(a[0]==a[n-1]){
-        cout<<0<<endl;
-        return 0;
-    }
-    set<int>ans;
-    for(int i  =1 ; i<=101;i++){
-        set<int>check;
-        for(int j = 0; j<n;j++){
-            check.insert(abs(a[j]-i));
-        }
-        if(check.find(0) != check.end()){
-            check.erase(0);
-        }
-//        for(auto z: check)cout<<z<<" ";
-//        cout<<endl<<endl;
-        if(check.size()==1)ans.insert(*check.begin());
-    }
-    if(ans.size()==0){
-        cout<<-1<<endl;
-    }
-    else cout<<*ans.begin()<<endl;
+                set<int>::iterator it;
+                vector<int>ans;
+                for(it = s.begin(); it!=s.end();it++)
+                {
+                    ans.pb(*it);
+                }
+                int len = s.size();
+                if(len == 1){
+                    cout<<0<<nl;
+                }
+                else if(len==2){
+                        int x = ans[1]-ans[0];
+                if(x%2 !=1)cout<<x/2<<nl;
+                 else    cout<<ans[1]-ans[0]<<nl;
+                }
+                else if(len==3){
+                    if(ans[2]-ans[1]==ans[1]-ans[0]){
+                        cout<<ans[2]-ans[1]<<nl;
+                    }
+                else cout<<-1<<nl;
+                }
+                else
+                    cout<<-1<<nl;
 
-
-return 0;
+     return 0;
 }
-
-
-
-
