78d4c45c8b12a5f4db398e10c55c9a952322364b
Towhid Zaman <towhid1zaman@gmail.com>
2019-05-11 15:13:00 +0600

    Add solution for problem `A - Eating Soup` Link: https://codeforces.com/contest/1163/problem/A Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1163/submission/53985113

---
+++ codeforces/1163/A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<double , double> pdd;
+typedef pair<LL , LL > pll;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<LL> vl;
+typedef long double ld;
+
+#define pb push_back
+#define lcm(a,b) a * (b / gcd(a, b))
+#define gcd(a,b) __gcd(a,b)
+#define nl "\n"
+#define inf 2e16
+#define MAX (int)2e5+5
+#define pi	acos(-1.0)
+#define ff first
+#define ss second
+#define mp make_pair
+#define check() {printf("OK\n");}
+#define mem(a,x) memset(a,x,sizeof(a))
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define UN(v) SORT(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define f ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+int main()
+{f
+
+
+        int n,m;
+        cin>>n>>m;
+        if(m==0){
+           cout<<1;
+             return 0;
+          }
+          if(n/2>=m) m = n - m;
+         cout<<n-m<<nl;
+          
+       
+
+           
+
+
+
+
+    return 0;
+}
