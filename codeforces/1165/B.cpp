75fd1e6a32ce5a4f7c532a40ce648f6dde3f43e1
Towhid Zaman <towhid1zaman@gmail.com>
2019-05-14 21:09:00 +0600

    Add solution for problem `B - Polycarp Training` Link: https://codeforces.com/contest/1165/problem/B Tags: data structures, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1165/submission/54109968

---
+++ codeforces/1165/B.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<double , double> pdd;
+typedef pair<LL , LL > pll;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<LL> vl;
+typedef long double LD;
+
+#define pb push_back
+#define lcm(a,b) a * (b / gcd(a, b))
+#define gcd(a,b) __gcd(a,b)
+#define nl "\n"
+#define inf 2e16
+#define MAX INT_MAX
+#define pi	acos(-1.0)
+#define ff first
+#define ss second
+#define mp make_pair
+#define check() {printf("OK\n");}
+#define mem(a,x) memset(a,x,sizeof(a))
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define UN(v) SORT(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define f() {ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);}
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+LL num,v[400005],res=0,x=1;
+int main()
+{f();
+
+        cin>>num;
+            for(int i=1;i<=num;i++){
+            cin>>v[i];
+        }
+        sort(v+1,v+num+1);
+        for(int i=1;i<=num;i++){
+            if(v[i]>=x){
+                x++;
+                res++;
+            }
+        }
+            cout<<res<<nl;
+
+
+
+
+
+
+
+
+
+	cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
+
