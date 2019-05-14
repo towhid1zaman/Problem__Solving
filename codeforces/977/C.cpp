49c22fa2b46062fcda793fdc1f05abba00fdcee0
Towhid Zaman <towhid1zaman@gmail.com>
2019-05-14 13:25:00 +0600

    Add solution for problem `C - Less or Equal` Link: https://codeforces.com/contest/977/problem/C Tags: sortings, *1200 Ref: https://codeforces.com/contest/977/submission/54086005

---
+++ codeforces/977/C.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<double , double> pdd;
+typedef pair<LL , LL > pll;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<LL> vl;
+typedef long double LD;
+
+#define pb push_back
+#define lcm(a,b) a * (b / gcd(a, b))
+#define gcd(a,b) __gcd(a,b)
+#define nl "\n"
+#define inf 2e16
+#define MAX (int)2e5+5
+#define pi	acos(-1.0)
+#define ff first
+#define ss second
+#define mp make_pair
+#define check() {printf("OK\n");}
+#define mem(a,x) memset(a,x,sizeof(a))
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define UN(v) SORT(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define f() {ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);}
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+int main()
+{f();
+
+        int n, k;
+        cin>>n>>k;
+        vector<int>v(n);
+        for(int i = 0; i<n;i++){
+            cin>>v[i];
+        }
+        sort(v.begin(),v.end());
+//            cout<<v[k]<<nl;
+//        for(int i = 0; i<n;i++){
+//            cout<<v[i]<<" ";
+//        }cout<<nl;
+        if(k==0){
+                if(v[0]>1){
+                    cout<<1<<nl;
+                }
+                else cout<<-1<<nl;
+                return 0;
+        }
+                if(v[k-1]== v[k]){
+                    cout<<-1<<nl;
+                }
+                else
+                    cout<<v[k-1]<<nl;
+
+
+
+	cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
+
