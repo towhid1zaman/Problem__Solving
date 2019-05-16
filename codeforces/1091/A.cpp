a41d20b8ec883815db3d84c2862b8f6f205a05be
Towhid Zaman <towhid1zaman@gmail.com>
2019-05-16 23:52:00 +0600

    Add solution for problem `A - New Year and the Christmas Ornament` Link: https://codeforces.com/contest/1091/problem/A Tags: brute force, implementation, math, *800 Ref: https://codeforces.com/contest/1091/submission/54252250

---
+++ codeforces/1091/A.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long LL;
+#define nl "\n"
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define MAX INT_MAX
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) a*(b/gcd(a, b))
+#define check() {printf("OK\n");}
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+#define mem(a,x) memset(a,x,sizeof(a))
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+#define f() {ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);}
+#define UNQ(v) SORT(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+
+
+int main()
+{f();
+            int y,b,r;
+            cin>>y>>b>>r;
+                        int mn = min(b, min(y+1, r-1));
+                        cout<<mn*3<<nl;
+
+       return 0;
+}
