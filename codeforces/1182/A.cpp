3c1439c57c40b5e59a670877ce91153e4eef3b6f
Towhid Zaman <towhid1zaman@gmail.com>
2019-06-12 12:43:00 +0600

    Add solution for problem `A - Filling Shapes` Link: https://codeforces.com/contest/1182/problem/A Tags: dp, math, *1000 Ref: https://codeforces.com/contest/1182/submission/55483598

---
+++ codeforces/1182/A.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long LL;
+#define nl "\n"
+#define X first
+#define Y second
+
+#define MAX INT_MAX
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+#define check() {printf("OK\n");}
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+
+#define mem(a,x) memset(a,x,sizeof(a))
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i <= (n); ++i)
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define f() {ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);}
+#define unq(v) srt(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define MOD 1000000007   // (int)1e9+7
+
+
+int main(){f();
+
+            int n;
+            cin>>n;
+           if(n%2==1)
+            {
+                cout<<0<<nl;
+            }
+            else
+            {
+                LL res = pow(2,n/2);
+                cout<<res<<nl;
+            }
+
+
+
+return 0;
+}
+
+
