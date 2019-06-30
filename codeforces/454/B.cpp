6e9b897cea8d7befa651785b846d2f0a3ecc481c
Towhid Zaman <towhid1zaman@gmail.com>
2019-06-30 12:02:00 +0600

    Add solution for problem `B - Little Pony and Sort by Shift` Link: https://codeforces.com/contest/454/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/454/submission/56296137

---
+++ codeforces/454/B.cpp
@@ -0,0 +1,62 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long LL;
+#define nl "\n"
+#define X first
+#define Y second
+
+#define MAX INT_MAX
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+#define check() {printf("OK\n");}
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+
+#define mem(a,x) memset(a,x,sizeof(a))
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i <= (n); ++i)
+#define rep2(i,a,b) for(int i =(a); i <=(b); ++i)
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define f() {ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);}
+#define unq(v) srt(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define MOD 1000000007   // (int)1e9+7
+
+
+int main(){f();
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        int cnt =0;
+        int pos;
+        for(int i =1; i<=n;i++){
+            cin>>v[i];
+            if(v[i]<v[i-1]){
+                cnt++;
+                pos = i;
+            }
+        }
+        if(cnt==0){
+            cout<<0<<nl;
+            return 0;
+        }
+        else if(cnt>1){
+            cout<<-1<<nl;
+            return 0;
+        }
+        else{
+            if(v[n]>v[1]){
+                cout<<-1<<nl;
+                return 0;
+            }
+            else{
+                cout<<n-pos+1<<nl;
+            }
+        }
+
+return 0;
+}
+
+
