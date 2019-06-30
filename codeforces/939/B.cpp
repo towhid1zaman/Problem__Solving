b7fdeb46ebbc96e3697b2d2502069b59e95d8fa3
Towhid Zaman <towhid1zaman@gmail.com>
2019-06-30 16:10:00 +0600

    Add solution for problem `B - Hamster Farm` Link: https://codeforces.com/contest/939/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/939/submission/56303176

---
+++ codeforces/939/B.cpp
@@ -0,0 +1,59 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long LL;
+#define nl "\n"
+#define X first
+#define Y second
+
+#define MAX INT_MAX
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+#define check() {printf("OK\n");}
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+
+#define mem(a,x) memset(a,x,sizeof(a))
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i <= (n); ++i)
+#define rep2(i,a,b) for(int i =(a); i <=(b); ++i)
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define f() {ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);}
+#define unq(v) srt(v),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define MOD 1000000007   // (int)1e9+7
+
+
+int main(){f();
+
+        LL n,k;
+        cin>>n>>k;
+        LL t=1,cnt;
+        LL a;
+        LL ans;
+        LL x,y,z;
+        cin>>a;
+        x = n/a;
+        ans = x;
+        y = n - (x*a);
+        z = y;
+        for(LL i = 2; i<=k;i++)
+        {
+          cin>>a;
+          x = n/a;
+          y = n - (x*a);
+            if(y<z){
+                ans = x;
+                z = y;
+                t = i;
+            }
+        }
+        cout<<t<<" "<<ans<<nl;
+
+
+return 0;
+}
+
+
+
