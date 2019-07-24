451b84f64a255b2ca976188616f70fe72be8e593
Towhid Zaman <towhid1zaman@gmail.com>
2019-07-25 00:18:00 +0600

    Add solution for problem `B - Radio Station` Link: https://codeforces.com/contest/918/problem/B Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/918/submission/57702183

---
+++ codeforces/918/B.cpp
@@ -0,0 +1,78 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define all(v) (v).begin(),(v).end()
+#define pd(x,k) cout<<setprecision(k)<<fixed<<x<<endl;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i <= (n); ++i)
+#define rep2(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+
+const int mod = 1000000007; // (int)1e9+7
+const int N = int(3e5) + 99;
+
+string solve()
+{
+    string s;
+    cin>>s;
+    set<char>sit;
+    for(int i = 0; i<s.length();i++)
+    {
+        sit.insert(s[i]);
+    }
+    if(sit.size()==1){
+            string res = "-1";
+        return res;
+    }
+    else
+        sort(all(s));
+        return s;
+}
+
+int main(){
+	IO
+     int n,m;
+     cin>>n>>m;
+     map<string, string>mip;
+     map<string,string>ans;
+     string a,b;
+     for(int i = 0; i<n;i++)
+     {
+         string a,b;
+         cin>>a>>b;
+         mip[b]=a;
+     }
+     vector<string>name;
+     vector<string>ipp;
+     vector<string>ip;
+     for(int i = 0; i<m;i++)
+     {
+            cin>>a>>b;
+            name.push_back(a);
+            ipp.push_back(b);
+            b.erase(b.begin()+b.length()-1);
+            ip.push_back(b);
+     }
+     for(int i = 0; i<m;i++)
+     {
+         cout<<name[i]<<" "<<ipp[i]<<" "<<"#"<<mip[ip[i]]<<endl;
+     }
+
+
+return 0;
+}
+
+
+
