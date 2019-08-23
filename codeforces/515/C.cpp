bdbe9d8504f7f715fd37f94d5aae3eddfbcb3659
Towhid Zaman <towhid1zaman@gmail.com>
2019-08-23 22:32:00 +0600

    Add solution for problem `C - Drazil and Factorial` Link: https://codeforces.com/contest/515/problem/C Tags: greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/515/submission/59364286

--- codeforces/515/C.cpp
+++ codeforces/515/C.cpp
@@ -1,37 +1,42 @@
-
-// </> : towhid1zaman
-
+//https://youtu.be/3I3l-x93kOY
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;typedef vector<ll> vll;
-#define fill(a,b) memset(a, b, sizeof (a))
-#define all(v) (v).begin(),(v).end()
-#define sp(k) cout<<setprecision(k)<<fixed;
-#define rep(i,a) for(int i=0; i<a;i++)
-#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
-#define irep(i,b,a) for(int i=(b);i>=(a);--i)
-#define minv(v) *min_element(all(v))
-#define maxv(v) *max_element(all(v))
-#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
-#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
 #define endl "\n"
-#define sqr(a) ((a)*(a))
-#define sz(a) int(a.size())
-#define ff first
-#define ss second
+#define F first
+#define S second
 #define pb push_back
-
-const double pi = acos(-1.0);
+#define mp make_pair
+#define all(v) (v).begin(),(v).end()
+#define sp(x,k) cout<<setprecision(k)<<fixed<<x<<endl;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
 const int mod = 1000000007; // (int)1e9+7
-const int maxn = 2000100;
+const int N = int(3e5) + 99;
+
 
 int main(){
-        _ios;
-        int n; cin >> n;
-        string s; cin >> s;
-        string res ="";
-        rep(i,n){
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+cout.tie(nullptr);
+cerr.tie(nullptr);
+
+	///0 ="",1="",2="2",3="3",4="223",5="5",6="53",7="7",8="7222",9="7332";
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	//sort(all(s)),reverse(all(s));
+	string res ="";
+	for(int i = 0; i<n;i++)
+    {
         if(s[i]=='0' or s[i]=='1'){
             res+="";
         }
@@ -62,9 +67,13 @@ int main(){
     }
    sort(all(res)),reverse(all(res));
     cout<<res<<endl;
- 
 
-        
+
+
+
+
+
+
 
 return 0;
 }
