714e065e4320a4752f05dd8a889689f1aebcf385
Towhid Zaman <towhid1zaman@gmail.com>
2019-08-31 12:40:00 +0600

    Add solution for problem `C - Book Reading` Link: https://codeforces.com/contest/1213/problem/C Tags: math, *1200 Ref: https://codeforces.com/contest/1213/submission/59789357

---
+++ codeforces/1213/C.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef unsigned long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
+#define all(v) (v).begin(),(v).end()
+#define sp(x,k) cout<<setprecision(k)<<fixed<<x<<endl;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+const int mod = 1000000007; // (int)1e9+7
+const int N = int(3e5) + 99;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    int n;
+    cin>>n;
+    while(n--){
+        ll n, m;
+        cin>>n>>m;
+        ll res = 0,mult=0;
+        rep(i,0,10){
+            mult+=((i*m)%10);
+        }
+        for (int i=0; i<=(n/m)%10; i++){
+           res+=((i*m)%10);
+        }
+        cout<<((n/m)/10)*mult+res<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
