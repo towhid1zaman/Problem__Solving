48cedfb1699ac8329e35c9ce8ec8fd3696c31af6
Towhid Zaman <towhid1zaman@gmail.com>
2019-09-04 21:28:00 +0600

    Add solution for problem `C - Bad Sequence` Link: https://codeforces.com/contest/1214/problem/C Tags: data structures, greedy, *1200 Ref: https://codeforces.com/contest/1214/submission/60031257

---
+++ codeforces/1214/C.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
+#define all(v) (v).begin(),(v).end()
+#define sp(x,k) cout<<setprecision(k)<<fixed<<x<<endl;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+const int mod = 1000000007; // (int)1e9+7
+const int N = int(3e5) + 99;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int bal = 0;
+    int mnbal = 0;
+    rep(i,0,n-1){
+        if(s[i]=='(')bal++;
+        else bal--;
+        mnbal = min(mnbal,bal);
+    }
+    if(bal==0 and mnbal>=-1)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
