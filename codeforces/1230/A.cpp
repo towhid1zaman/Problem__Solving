bb189b764e76cbcea12b2a4c0c89e9c27d6549f5
Towhid Zaman <towhid1zaman@gmail.com>
2019-09-23 20:34:00 +0600

    Add solution for problem `A - Dawid and Bags of Candies` Link: https://codeforces.com/contest/1230/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1230/submission/61138436

---
+++ codeforces/1230/A.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
+#define all(v) (v).begin(),(v).end()
+#define sp(x,k) cout<<setprecision(k)<<fixed<<x<<endl;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+const int mod = 1000000007; // (int)1e9+7
+const int N = int(3e5) + 99;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if((a+b)==(c+d) || (a+c)==(b+d) || (b+c)==(a+d) || a==(b+c+d) || b==(a+c+d) || c==(a+b+d) || d==(a+c+b)){
+            cout<<"YES"<<endl;
+    }
+    else{
+            cout<<"NO"<<endl;
+    }
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
