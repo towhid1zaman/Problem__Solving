0598559a8af1bb7aebf8d9623be4b4bb14440539
Towhid Zaman <towhid1zaman@gmail.com>
2019-10-11 09:17:00 +0600

    Add solution for problem `A - Little Elephant and Function` Link: https://codeforces.com/contest/221/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/221/submission/62338354

---
+++ codeforces/221/A.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
+#define all(v) (v).begin(),(v).end()
+#define sp(x,k) cout<<setprecision(k)<<fixed<<x<<endl;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+const int mod = 1000000007; // (int)1e9+7
+const int N = 200010;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    int n;
+    cin>>n;
+    int v[n+1];
+    for(int i = 1;i<=n;i++){
+        v[i] = i;
+    }
+    int x =n;
+    while(true)
+    {
+        if(x==1)break;
+        swap(v[x-1],v[x]);
+        x-=1;
+    }
+//    cout<<v[v.size()-1]<<" ";
+//    sort(all(v));
+    for(int i = 1; i<=n;i++){
+        cout<<v[i]<<" ";
+    }
+
+
+
+return 0;
+}
+
+
+
