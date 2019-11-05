72e2e30d6000e7a5f99d290ed01325a82a01bdc2
Towhid Zaman <towhid1zaman@gmail.com>
2019-11-06 00:58:00 +0600

    Add solution for problem `D - Binary String Minimizing` Link: https://codeforces.com/contest/1256/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/1256/submission/64330186

---
+++ codeforces/1256/D.cpp
@@ -0,0 +1,86 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair <int, int> pii;
+typedef vector<pii>vip;
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+const int mod = 1000000007; // (int)1e9+7
+const int N = 200100;
+
+void solve()
+{
+    string s;
+    ll n,k;
+    cin>>n>>k;
+    cin>>s;
+    vector<ll>zero;
+    set<ll>output;
+    for(int i = 0; i<n;i++)
+    {
+        if(s[i]=='0')zero.pb(i+1);
+    }
+    int curpos=1;
+    int totrep=0;
+    for(int i = 0; i<zero.size();i++)
+    {
+        if(k>=zero[i]-curpos)
+        {
+            output.insert(curpos);
+            k -= zero[i] - curpos;
+            curpos++;
+        }
+        else
+        {
+            output.insert(zero[i]-k);
+            k=0;
+        }
+        totrep++;
+        if(k==0)break;
+    }
+    for(int i = totrep; i<zero.size();i++)
+    {
+        output.insert(zero[i]);
+    }
+    for(int i = 1; i<=n;i++)
+    {
+        if(output.count(i)>0){
+            cout<<'0';
+        }
+        else cout<<'1';
+    }
+    cout<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        solve();
+    }
+
+
+
+return 0;
+}
+
+
+
+
