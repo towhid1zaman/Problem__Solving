bbe903821add98ac2ebd714edcaf8bf2a031c28b
Towhid Zaman <towhid1zaman@gmail.com>
2019-11-09 18:37:00 +0600

    Add solution for problem `C - Building Permutation` Link: https://codeforces.com/contest/285/problem/C Tags: greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/285/submission/64582996

--- codeforces/285/C.cpp
+++ codeforces/285/C.cpp
@@ -1,66 +1,49 @@
-
-// </> : towhid1zaman
-
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef pair< int, int > pii;
+typedef pair <int, int> pii;
 typedef vector<pii>vip;
-typedef pair< pii, int > ppi;
-#define fill(a,b) memset(a,b,sizeof(a))
+#define pb push_back
 #define all(v) (v).begin(),(v).end()
 #define sp(k) cout<<setprecision(k)<<fixed;
-#define rep(i,a) for(int i=0;i<a;i++)
-#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
-#define irep(i,b,a) for(int i=(b);i>=(a);--i)
-#define minv(v) *min_element(all(v))
-#define maxv(v) *max_element(all(v))
-#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
-#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
 #define endl "\n"
-#define sqr(a) ((a)*(a))
-#define sz(a) int(a.size())
-#define ff first
-#define ss second
-#define pb push_back
 
-const double pi = acos(-1.0);
 const int mod = 1000000007; // (int)1e9+7
-const int inf = 0x3f3f3f3f;// (int)3e18;
-const int maxn = 2000100;
-
-
-void task(){
-    int n; cin >> n;
-    vi a(n);
-    for(int i = 0; i<n; i++){
-        cin >> a[i];
-    }
-    sort(all(a));
-    ll cnt = 0;
-    for(int i = 0; i<n; i++){
-        int ind = i+1;
-        int val = a[i];
-        if(ind>val){
-            if(val>0)cnt+=(ind-val);
-            if(val==0)cnt+=ind;
-            if(val<0){
-                cnt+=abs(val);
-                cnt+=ind;
-            }
-        }
-        else if(ind<val) cnt+=(val-ind);
-    }
-    cout << cnt << endl;
-}
-
+const int N = 300100;
+int a[N];
 int main(){
-        _
-        int T = 1; //cin >> T;
-        while(T--){
-            task();
-        }
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n;i++){
+        cin>>a[i];
+    }
+    ll ans = 0;
+    sort(a+1,a+n+1);
+    for(int i = 1; i<=n;i++){
+        ans+= abs(a[i] - i);
+        a[i] = i;
+    }
+    cout<<ans<<endl;
+
+
+
+
+
 return 0;
 }
+
+
+
+
