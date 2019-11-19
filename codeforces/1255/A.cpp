7290288f9fb6ead042232b5c53bee9a616eaeaa6
Towhid Zaman <towhid1zaman@gmail.com>
2019-11-20 00:06:00 +0600

    Add solution for problem `A - Changing Volume` Link: https://codeforces.com/contest/1255/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1255/submission/65393213

---
+++ codeforces/1255/A.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a,b) for(int i =(a); i <=(b); ++i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+#define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+
+int dx4[ ] = {0,0,1,-1};
+int dy4[ ] = {1,-1,0,0};
+int dx8[ ] = {0,0,1,1,1,-1,-1,-1};
+int dy8[ ] = {1,-1,1,-1,0,0,-1,1};
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 200100;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        int d = abs(a-b);
+        int ans  = d/5;
+        d = d%5;
+        ans+=d/2;
+        ans+=d%2;
+        cout<<ans<<endl;
+    }
+
+
+return 0;
+}
+
+
+
+
