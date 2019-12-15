1d1b152e563084905b24dd9aa58e743ba7ebc528
Towhid Zaman <towhid1zaman@gmail.com>
2019-12-16 02:02:00 +0600

    Add solution for problem `A - Marks` Link: https://codeforces.com/contest/152/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/152/submission/66990348

---
+++ codeforces/152/A.cpp
@@ -0,0 +1,70 @@
+    #include "bits/stdc++.h"
+    using namespace std;
+    typedef long long ll;
+    typedef vector<int> vi;
+    typedef vector<ll> vll;
+    #define all(v) (v).begin(),(v).end()
+    #define sp(k) cout<<setprecision(k)<<fixed;
+    #define rep(i,a,b) for(int i=(a);i<=(b);++i)
+    #define irep(i,b,a) for(int i=(b);i>=(a);--i)
+    #define minv(v) *min_element(v.begin(),v.end())
+    #define maxv(v) *max_element(v.begin(),v.end())
+    #define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+    #define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+    #define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+    #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #define endl "\n"
+    #define ff first
+    #define ss second
+    #define pb push_back
+
+    int dx4[ ] = {0,0,1,-1};
+    int dy4[ ] = {1,-1,0,0};
+    int dx8[ ] = {0,0,1,1,1,-1,-1,-1};
+    int dy8[ ] = {1,-1,1,-1,0,0,-1,1};
+
+    const double pi = acos(-1.0);
+    const int mod = 1000000007; // (int)1e9+7
+    const int maxn = 200100;
+    int main(){
+
+    IOS;
+    int n,m;
+    cin>>n>>m;
+    int res = 0;
+    string s;
+    vector<string>v;
+    int mix[100]={0};
+    rep(i,0,n-1){
+      cin>>s;
+      v.pb(s);
+    }
+    rep(i,0,m-1){
+        int mx = 0;
+        rep(j,0,n-1){
+            mx = max(v[j][i]-'0',mx);
+        }
+        mix[i] = mx;
+    }
+    rep(i,0,n-1){
+        //int chek = 0;
+        rep(j,0,m-1){
+            if(v[i][j]-'0'==mix[j]){
+                res++;
+                break;
+            }
+        }
+       // res+=chek;
+    }
+    cout<<res<<endl;
+
+
+
+
+
+    return 0;
+    }
+
+
+
+
