0a020c79ad01b3ccca07c113a2a5d6e6ec610f96
Towhid Zaman <towhid1zaman@gmail.com>
2019-12-15 22:15:00 +0600

    Add solution for problem `A - Suffix Three` Link: https://codeforces.com/contest/1281/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1281/submission/66980599

---
+++ codeforces/1281/A.cpp
@@ -0,0 +1,57 @@
+    #include "bits/stdc++.h"
+    using namespace std;
+    typedef long long ll;
+    typedef vector<int> vi;
+    typedef vector<ll> vll;
+    #define all(v) (v).begin(),(v).end()
+    #define sp(k) cout<<setprecision(k)<<fixed;
+    #define rep(i,a,b) for(int i=(a);i<=(b);++i)
+    #define irep(i,b,a) for(int i=(b);i>=(a);--i)
+    #define minv(v) *min_element(v.begin(),v.end())
+    #define maxv(v) *max_element(v.begin(),v.end())
+    #define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+    #define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+    #define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+    #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #define endl "\n"
+    #define ff first
+    #define ss second
+    #define pb push_back
+
+    int dx4[ ] = {0,0,1,-1};
+    int dy4[ ] = {1,-1,0,0};
+    int dx8[ ] = {0,0,1,1,1,-1,-1,-1};
+    int dy8[ ] = {1,-1,1,-1,0,0,-1,1};
+
+    const double pi = acos(-1.0);
+    const int mod = 1000000007; // (int)1e9+7
+    const int maxn = 200100;
+
+    int main(){
+
+    IOS;
+    int N;
+    cin>>N;
+    while(N--){
+      string s;
+      cin>>s;
+      int n = s.size();
+      if(s[n-1]=='o' and s[n-2]=='p'){
+        cout<<"FILIPINO"<<endl;
+      }
+      else if(s[n-1]=='a' and s[n-2]=='d' and s[n-3]=='i' and s[n-4]=='n' and s[n-5]=='m'){
+        cout<<"KOREAN"<<endl;
+      }
+      else cout<<"JAPANESE"<<endl;
+    }
+    
+
+
+
+
+    return 0;
+    }
+
+
+
+
