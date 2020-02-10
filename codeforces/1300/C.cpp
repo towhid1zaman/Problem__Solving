22cb1fbb75454bc742161fffed3278e9696b8d02
Towhid Zaman <towhid1zaman@gmail.com>
2020-02-10 23:42:00 +0600

    Add solution for problem `C - Anu Has a Function` Link: https://codeforces.com/contest/1300/problem/C Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1300/submission/70747132

---
+++ codeforces/1300/C.cpp
@@ -0,0 +1,64 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define sz(x) (int)x.size()
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000010;
+const ll N = 1e18;
+int Pref[maxn],Suf[maxn],v[maxn];
+int main(){
+        _ios;
+
+        int n;cin>>n;
+        rep1(i,1,n){
+            cin>>v[i];
+            Pref[i] = Pref[i-1]|v[i];
+        }
+        irep(i,n,1){
+            Suf[i] = Suf[i+1]|v[i];
+        }
+        int res=-1,bs=0;
+        rep1(i,1,n){
+            int x = v[i];
+            int y = Pref[i-1]|Suf[i+1];
+            if((x|y)-y>res){
+                res = (x|y)-y;
+                bs = i;
+            }
+        }
+        cout<<v[bs]<<" ";
+        rep1(i,1,n){
+            if(i!=bs)cout<<v[i]<<" ";
+        }
+        cout<<endl;
+
+
+
+return 0;
+}
+
+
+
+
+
+
