41f48d1fec2094dff250d5be67a2c244c838ab56
Towhid Zaman <towhid1zaman@gmail.com>
2020-03-19 22:55:00 +0600

    Add solution for problem `D1 - Prefix-Suffix Palindrome (Easy version)` Link: https://codeforces.com/contest/1326/problem/D1 Tags: hashing, string suffix structures, strings, *1500 Ref: https://codeforces.com/contest/1326/submission/73730071

--- codeforces/1326/D1.cpp
+++ codeforces/1326/D1.cpp
@@ -4,16 +4,19 @@
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
+typedef unsigned long long ull;
 typedef vector<int> vi;typedef vector<ll> vll;
-#define fill(a,b) memset(a, b, sizeof (a))
+#define fill(a) memset(a, 0, sizeof (a))
 #define all(v) (v).begin(),(v).end()
 #define sp(k) cout<<setprecision(k)<<fixed;
 #define rep(i,a) for(int i=0; i<a;i++)
 #define rep1(i,a,b) for(int i=(a);i<=(b);++i)
 #define irep(i,b,a) for(int i=(b);i>=(a);--i)
-#define minv(v) *min_element(all(v))
-#define maxv(v) *max_element(all(v))
-#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
 #define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 #define sqr(a) ((a)*(a))
@@ -22,80 +25,67 @@ typedef vector<int> vi;typedef vector<ll> vll;
 #define ss second
 #define pb push_back
 
+const int INF = 1e9 + 10;
 const double pi = acos(-1.0);
 const int mod = 1000000007; // (int)1e9+7
 const int maxn = 2000100;
-
-int phi[maxn];
-int  kmp_prefix_function(string s){
-    int n = s.size();
-    int index = 0;
-    phi[0]=0;
-    for(int i = 1; i<n; i++){
-        while(index>0 and s[index] !=s[i]){
-            index = phi[index-1];
-        }
-        if(s[i] == s[index]){
-            index++;
-        }
-        phi[i] = index;
-    }
-    return phi[n-1];
-}
-
-
-int largest_pref_palin(string s){
-    s = s+"?";
+bool ispalin(string s){
     string t = s;
     reverse(all(t));
-    s+=t;
-    return kmp_prefix_function(s);
+    return (t==s);
 }
-
 int main(){
         _ios;
 
         int T; cin >> T;
         while(T--){
             string s; cin >> s;
-            int n = sz(s);
-
             int l = 0;
-            for(int i = 0,j= n-1;i<j; i++,j--){
-                if(s[i]==s[j]){
+            int r = sz(s) - 1;
+            int res = 0;
+            string a,b,c;
+            while(l<r){
+                if(s[l]==s[r]){
+                    a+=s[l];
+                    b+=s[r];
                     l++;
+                    r--;
                 }
-                else{
-                    break;
+                else break;
+            }
+            //cout<<a<<" "<<b<<endl;
+            //cout<<l<<" "<<r<<endl;
+            string chek = s.substr(l,r-l+1);
+            rep(i,sz(chek)){
+                string d = chek.substr(0,i+1);
+                string e = chek.substr(i,sz(s)-i);
+                if(ispalin(d)){
+                    //cout<<"hoiche"<<endl;
+                    if(res<sz(d)){
+                        res = sz(d);
+                        c = d;
+                    }
+                }
+                if(ispalin(e)){
+                    if(res<sz(e)){
+                        res = sz(e);
+                        c = e;
+                    }
                 }
             }
-
-            //remaining   s[l......n-l-1]
-            string rem = s.substr(l,n-2*l);
-            string ans = s.substr(0,l);
-
-            //if remaining exist
-
-            if(rem.size()){
-                int l_pref = largest_pref_palin(rem);
-                reverse(all(rem));
-                int l_suf = largest_pref_palin(rem);
-
-                if(l_pref>l_suf){
-                    reverse(all(rem));
-                    ans+=rem.substr(0,l_pref);
-                }
-                else{
-                    //no need to reverse, cause its already reversed
-                    ans+=rem.substr(0,l_suf);
-                }
-            }
-
-            ans+= s.substr(n-l, l);
+            reverse(all(b));
+            //reverse(all(c));
+            string ans = a+c+b;
             cout<<ans<<endl;
+
         }
 
 
 return 0;
 }
+
+
+
+
+
 
