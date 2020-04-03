27d02d9e564c875cbf3772fb2381863ed9edf159
Towhid Zaman <towhid1zaman@gmail.com>
2020-04-03 21:08:00 +0600

    Add solution for problem `D - Dreamoon Likes Sequences` Link: https://codeforces.com/contest/1330/problem/D Tags: bitmasks, constructive algorithms, dp, math, *1700 Ref: https://codeforces.com/contest/1330/submission/75372783

---
+++ codeforces/1330/D.cpp
@@ -0,0 +1,58 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define inf (1<<29)
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+ll log(ll x){
+    return log2(x);
+}
+int main(){
+        _ios;
+
+        int T; cin >> T;
+        while(T--){
+            ll d,m; cin >> d >> m;
+            ll chek = log(d);
+            ll res = 1;
+            rep1(i,1,chek){
+                res = (res*(1+(1<<(i-1)))%m)%m;
+            }
+            res = (res*(d-(1<<chek)+2)%m)%m;
+            res--; res+=m;
+            res%=m;
+            cout<<res<<endl;
+        }
+
+return 0;
+}
+
+
+
+
+
