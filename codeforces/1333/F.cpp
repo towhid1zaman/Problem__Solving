edace8ce1ff3fc8c2bfab73e43c9756a0540b5ab
Towhid Zaman <towhid1zaman@gmail.com>
2020-04-09 16:11:00 +0600

    Add solution for problem `F - Kate and imperfection` Link: https://codeforces.com/contest/1333/problem/F Tags: greedy, implementation, math, number theory, sortings, two pointers, *2200 Ref: https://codeforces.com/contest/1333/submission/75977360

---
+++ codeforces/1333/F.cpp
@@ -0,0 +1,57 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define inf (1<<29)
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+int main(){
+        _ios;
+
+        int n; cin >> n;
+        vi ans;
+        for(int i = 2; i<=n; i++){
+            bool good  = false;
+            for(int d = 2; d*d<=i; d++){
+                if(i%d==0){
+                    ans.pb(i/d);
+                    good = true;
+                    break;
+                }
+            }
+            if(good==false)ans.pb(1);
+        }
+        sort(all(ans));
+        for(auto x : ans){
+            cout<<x<<' ';
+        }
+        cout<<endl;
+
+
+
+return 0;
+}
