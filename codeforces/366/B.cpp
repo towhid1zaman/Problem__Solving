46e7effa6e2a2cb8130df71591fbe84d180e5fcf
Towhid Zaman <towhid1zaman@gmail.com>
2020-04-22 21:48:00 +0600

    Add solution for problem `B - Dima and To-do List` Link: https://codeforces.com/contest/366/problem/B Tags: brute force, implementation, *1200 Ref: https://codeforces.com/contest/366/submission/77668941

---
+++ codeforces/366/B.cpp
@@ -0,0 +1,49 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a,b) memset(a, b, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+int a[100000],sum[100000];
+int main(){
+        _ios;
+        
+        int n,k; cin >> n >> k;
+        for(int i = 0; i<n; i++){
+            cin >> a[i];
+            sum[i%k]+=a[i];
+        }
+        int res = 0;
+        for(int i = 1; i<k; i++){
+            if(sum[i]<sum[res]){
+                res = i;
+            }
+        }
+        cout<<res+1<<endl;
+
+
+
+return 0;
+}
