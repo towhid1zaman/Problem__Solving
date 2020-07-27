848553049b9f7532fd36f5986cbb5459ea610e75
Towhid Zaman <towhid1zaman@gmail.com>
2020-07-27 14:38:00 +0600

    Add solution for problem `A - k-rounding` Link: https://codeforces.com/contest/858/problem/A Tags: brute force, math, number theory, *1100 Ref: https://codeforces.com/contest/858/submission/88110427

---
+++ codeforces/858/A.cpp
@@ -0,0 +1,76 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+ll gcd(ll a,ll b){
+    return b ? gcd(b,a%b) : a;
+}
+ll lcm(ll a, ll b){
+    return a/ gcd(a,b)*b;
+}
+ll bxpo(ll x,ll n){
+    ll res = 1;
+    while(n>0){
+        if(n%2==1){
+            res = res*x;
+        }
+        x = x*x;
+        n = n/2;
+    }
+    return res;
+}
+void task(){
+    /* find min x such that x%(10^k)==0 && x%n==0
+        It is LCM(n, 10^k)
+    */
+    ll n, k; cin >> n >> k;
+    cout << lcm(n,bxpo(10,k))<<endl;
+    
+
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+
