aba51d66ea5c5fa257d86c70309e590b629b3deb
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-03 09:05:00 +0600

    Add solution for problem `D - Power Products` Link: https://codeforces.com/contest/1247/problem/D Tags: number theory, *1800 Ref: https://codeforces.com/contest/1247/submission/88768678

--- codeforces/1247/D.cpp
+++ codeforces/1247/D.cpp
@@ -12,7 +12,7 @@ typedef pair< pii, int > ppi;
 #define fill(a,b) memset(a,b,sizeof(a))
 #define all(v) (v).begin(),(v).end()
 #define sp(k) cout<<setprecision(k)<<fixed;
-#define rep(i,a) for(int i=0;i<a;i++)
+#define rep(i,a) for(ll i=0;i<a;i++)
 #define rep1(i,a,b) for(int i=(a);i<=(b);++i)
 #define irep(i,b,a) for(int i=(b);i>=(a);--i)
 #define minv(v) *min_element(all(v))
@@ -30,54 +30,72 @@ const double pi = acos(-1.0);
 const int mod = 1000000007; // (int)1e9+7
 const int inf = 0x3f3f3f3f;// (int)3e18;
 const int maxn = 200005;
-
-//binary exponentiation
-ll bxpo(ll x,ll n){
-    ll res = 1;
-    while(n>0){
-        if(n%2==1){
-            res = res*x;
+//smallest prime factor of a number
+int lpf[1000005];
+void sieve(){
+    for(ll i=2;i<=1000000;i++){
+        if(lpf[i]==0){
+      		lpf[i]=i;
+        for(ll j=i*2;j<=1000000;j+=i){
+            if(lpf[j]==0)lpf[j]=i;
         }
-        x = x*x;
-        n = n/2;
     }
-    return res;
+  }
 }
-//prime factorization
-vector<pair<ll,ll>>factorize(ll a){
-    vector<pair<ll,ll> > pf;
-    for (int i = 2; i * i <= a; ++i) {
-        if (a % i) continue;
-        pf.emplace_back(i, 0);
-        while (a % i == 0) {
-            ++pf.back().second;
-            a /= i;
-        }
-    }
-    if (a > 1) {
-        pf.emplace_back(a, 1);
-    }
-    return pf;
+vector< pair<ll,ll> >get_factorization(ll n, ll k){
+	vector< pair<ll,ll> >factors;
+	ll prev = -1,cur = lpf[n], ind = -1;
+	while(n!=1){
+		if(prev!=cur){
+			if(ind!=-1){
+				if(factors[ind].second % k == 0){
+					factors.pop_back();
+					ind--;
+				}
+				else{
+					factors[ind].second%=k;
+				}
+			}
+			factors.push_back(make_pair(cur,1));
+			ind++;
+		}
+		else
+			factors[ind].second++;
+		prev = cur;
+		n /= cur;
+		cur = lpf[n];
+	}
+	if(ind!=-1){
+		if(factors[ind].second%k== 0){
+			factors.pop_back();
+			ind--;
+		}
+		else{
+			factors[ind].second%=k;
+		}
+	}
+	return factors;
 }
 void task(){
-    ll n,k; cin >> n >> k;
-    ll ans = 0;
-    std::map<ll, ll> freq;
-    
-    while(n--){
-        ll a; cin >> a;
-        std::vector<pair<ll,ll>>pf = factorize(a);
-        ll me = 1, other = 1;
-        for (auto &x: pf) {
-            ll p = x.first;
-            ll c = x.second;
-            me *= pow(p, c % k); 
-            other *= pow(p, (k - c % k) % k);
-        }
-        ans+=freq[other];
-        ++freq[me];
-    }
-    cout << ans << endl;
+
+ 	int n,k; cin >> n >> k;
+ 	vll a(n);
+ 	rep(i,n) cin >> a[i];
+
+ 	std::map<vector< pair<ll,ll> >, ll> L_store;
+
+ 	ll ans = 0;
+ 	rep(i,n){
+ 		vector< pair<ll,ll> > List = get_factorization(a[i],k);
+ 		vector< pair<ll,ll> > L(List);
+ 		for(auto &it:L){
+ 			it.second = k - it.second;
+ 		}
+ 		ans+=L_store[L];
+ 		L_store[List]++;
+ 	}
+
+ 	cout << ans << endl;
 
 }
 
@@ -87,7 +105,7 @@ int main(){
         freopen("inputf.in", "r", stdin);
         freopen("outputf.in", "w", stdout);
         #endif
-
+        sieve();
         int T = 1; //cin >> T;
         while(T--){
             task();
@@ -97,4 +115,3 @@ return 0;
 }
 
 
-        
