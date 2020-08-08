9795823aaea031ae21e58693f15394f1a9d46dd9
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-08 18:34:00 +0600

    Add solution for problem `A - Table` Link: https://codeforces.com/contest/359/problem/A Tags: constructive algorithms, greedy, implementation, *1000 Ref: https://codeforces.com/contest/359/submission/89334403

---
+++ codeforces/359/A.cpp
@@ -0,0 +1,76 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+int n,m;
+int g[200][200];
+bool vis[200][200];
+void task(){
+    cin >> n >> m;
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            cin >> g[i][j];
+        }
+    }
+    vis[0][0]=1,vis[n-1][0]=1,vis[0][m-1]=1,vis[n-1][m-1]=1;
+    int fr=0, fc=0,lr=0,lc=0;
+    for(int i = 0;i<n; i++){
+        for(int j = 0; j<m; j++){
+            if(!vis[i][j] and g[i][j]){
+                if(i==0)fr++;
+                else if(j==0 )fc++;
+                else if(i==n-1)lr++;
+                else if(j==m-1)lc++;
+            }
+        }
+    }
+    if(fr>0 or fc>0)cout << 2 << endl;
+    else if(lr>0 or lc>0)cout << 2 << endl;
+    else cout << 4 << endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+    
