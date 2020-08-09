76c8e0836645321a9945c4ca26d3375a3c7f7c05
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-09 23:59:00 +0600

    Add solution for problem `C - Cyclic Permutations ` Link: https://codeforces.com/contest/1391/problem/C Tags: combinatorics, dp, graphs, math, *1500 Ref: https://codeforces.com/contest/1391/submission/89458754

---
+++ codeforces/1391/C.cpp
@@ -0,0 +1,68 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+inline ll MOD(ll x)
+{
+    ll y = x % mod;
+    return (y >= 0) ? y: y+mod; // if -ve, simply add M
+}
+
+void task(){
+    // n! - 2^(n-1)
+    int n, factorial = 1, power2 = 1;
+    
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        factorial = 1LL * factorial * i % mod;
+        if (i < n) {
+            power2 = power2 * 2 % mod;
+        }
+    }
+    cout << MOD(factorial - power2)<< endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+
