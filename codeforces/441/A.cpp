f6f7c36267da9166073d26e502be3060aeff04c0
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-09 18:45:00 +0600

    Add solution for problem `A - Valera and Antique Items` Link: https://codeforces.com/contest/441/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/441/submission/89400122

---
+++ codeforces/441/A.cpp
@@ -0,0 +1,71 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+ 
+void task(){
+ 	int n,v; cin >> n >> v;
+ 	int ans = 0;
+ 	vi ini;
+ 	rep(j,n){
+ 		int k; cin >> k;
+ 		bool f = 0;
+ 		for(int i = 0; i<k; i++){
+ 			int p; cin >> p;
+ 			if(p<v and !f){
+ 				ini.pb(j+1);
+ 				f = 1;
+ 			}
+ 		}
+ 	}
+ 	cout << ini.size() << endl;
+ 	for(int i = 0; i<ini.size(); i++){
+ 		cout <<ini[i]<<' ';
+ 	}
+ 	cout << endl;
+ }
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+
