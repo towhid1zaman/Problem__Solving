32ec6cb72555e17ed3739e002264ed87a6271f42
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-13 10:52:00 +0600

    Add solution for problem `C - Appleman and Toastman` Link: https://codeforces.com/contest/462/problem/C Tags: implementation, sortings, *1200 Ref: https://codeforces.com/contest/462/submission/89760440

--- codeforces/462/C.cpp
+++ codeforces/462/C.cpp
@@ -1,82 +1,75 @@
 
-
 // </> : towhid1zaman
 
-//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
-//#pragma GCC target("avx,avx2,fma") 
-#pragma comment (linker,"/STACK:16777216")
 #include "bits/stdc++.h"
 using namespace std;
-
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-
-#define endl "\n"
-#define sqr(a) ((a)*(a))
-#define sz(x) ((int)(x).size())
-#define ff first
-#define ss second
-#define pb push_back
-#define eb emplace_back
-#define deb(x) cout<<#x <<"="<<x<<endl;
-#define present(v,x) ((v).find(x)!=(v).end())
-#define _ ios::sync_with_stdio(0);cin.tie(0);
-#define sp(k) cout<<setprecision(k)<<fixed;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
 #define fill(a,b) memset(a,b,sizeof(a))
 #define all(v) (v).begin(),(v).end()
-
+#define sp(k) cout<<setprecision(k)<<fixed;
 #define rep(i,a) for(int i=0;i<a;i++)
 #define rep1(i,a,b) for(int i=(a);i<=(b);++i)
 #define irep(i,b,a) for(int i=(b);i>=(a);--i)
-
 #define minv(v) *min_element(all(v))
 #define maxv(v) *max_element(all(v))
 #define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
-#define yn(k) if(k)puts("YES");else puts("NO");
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
 
-vi mods = {1000000007, 1000000009, 998244353};
 const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
 const int inf = 0x3f3f3f3f;// (int)3e18;
-const int maxn = 200005;
-const int mod = mods[0];
-
+const int maxn = 300005;
 /*
- *
- */
+	every time add sum the last all element except first element.
+	brute
+*/
 
+ll a[maxn], pref[maxn];
 void task(){
-        int n; cin >> n;
-        std::vector<int>a(n);
-        ll tot = 0, ans = 0;
-        rep(i, n)cin >> a[i], tot+=a[i];
-        ans+=tot;
-        sort(all(a));
-        rep(i, n-1) ans+=tot, tot-=a[i];
-        cout << ans << endl;
+ 	int n; cin >> n;
+ 	ll sum = 0;
+ 	rep(i,n){
+ 		cin >> a[i];
+ 		sum+=a[i];
+ 	}
+ 	if(n==1){
+ 		cout << sum << endl;
+ 		return;
+ 	}
+ 	sort(a,a+n);
+ 	ll ans = 0;
+ 	for(int i = 0; i<n; i++){
+ 		ans+=sum;
+ 		sum-=a[i];
+ 		if(i!=n-1)ans+=a[i];
+ 	}
+ 	cout << ans << endl;
 }
 
 int main(){
         _
-#ifdef OJ
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-#endif
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
 
         int T = 1; //cin >> T;
-        for(int __ = 1; __ <= T; __++){
-            //cout <<"Case "<<__<<": ";
+        while(T--){
             task();
         }
-
-
-/*array size ?
- *n==1 ?
- *different approach ?
-*/        
+        
 return 0;
 }
+
+
