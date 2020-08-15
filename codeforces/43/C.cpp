c5457ab6fdce2d115c7291133722c5161dd75345
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-15 12:29:00 +0600

    Add solution for problem `C - Lucky Tickets` Link: https://codeforces.com/contest/43/problem/C Tags: greedy, *1300 Ref: https://codeforces.com/contest/43/submission/89992345

---
+++ codeforces/43/C.cpp
@@ -0,0 +1,69 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+*after concatenate two numbers we have check it is divisible by 3 or not
+*count such pair
+*if a number is divisible by 3 then sum of digit also be divisible by 3
+*/
+ 
+void task(){
+ 	int n; cin >> n;
+ 	vi a(n);
+ 	for(auto &v:a)cin >> v;
+ 	int one=0,two=0, zero = 0;
+ 	for(auto &v:a){
+ 		if(v%3==0)zero++;
+ 		else if(v%3==1)one++;
+ 		else if(v%3==2)two++;
+ 	}
+ 	int ans = min(one, two) + zero/2;
+ 	cout << ans << endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+
