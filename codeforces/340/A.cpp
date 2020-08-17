a63f1f789401a3a2ffe0314a1c9d31448e9dab2d
Towhid Zaman <towhid1zaman@gmail.com>
2020-08-17 20:20:00 +0600

    Add solution for problem `A - The Wall` Link: https://codeforces.com/contest/340/problem/A Tags: math, *1200 Ref: https://codeforces.com/contest/340/submission/90236650

---
+++ codeforces/340/A.cpp
@@ -0,0 +1,65 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+//#pragma GCC optimize("Ofast")  
+//#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef vector<pii>vip;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+ll gcd(ll a,ll b){
+    return b ? gcd(b,a%b) : a;
+}
+ll lcm(ll a, ll b){
+    return a/ gcd(a,b)*b;
+}
+void task(){
+ 	ll x, y, a, b;
+	cin >> x >> y >> a >> b;
+	ll z = lcm(x, y);
+	cout << b/z - (a-1)/z << endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+return 0;
+}
+
+
