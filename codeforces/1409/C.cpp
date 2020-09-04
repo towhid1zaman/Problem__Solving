e267386f7eb0157660d2813e1eb2a2fa50b15bfe
Towhid Zaman <towhid1zaman@gmail.com>
2020-09-04 22:24:00 +0600

    Add solution for problem `C - Yet Another Array Restoration` Link: https://codeforces.com/contest/1409/problem/C Tags: brute force, math, number theory, *1200 Ref: https://codeforces.com/contest/1409/submission/91870447

---
+++ codeforces/1409/C.cpp
@@ -0,0 +1,91 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+ *
+ */
+ll possible(ll s,ll d,ll n,ll x,ll y){
+	ll cnt = 0,p=0;
+	bool f1=0,f2=0;
+    for(p=s;cnt<n;p+=d,cnt++){
+        if(p==x)f1=1;
+        if(p==y)f2=1;
+    }
+    if(f1 and f2)return p;
+    else return inf;
+}
+void task(){
+	ll n,x,y; cin >>n >> x >> y;
+	ll mn=inf,a=0,b=0;
+	rep1(i,1,x)rep1(j,1,y-x){
+           if(possible(i,j,n,x,y)<mn)mn=possible(i,j,n,x,y),a=i,b=j;
+	}
+	rep1(i,1,n){
+		cout << a; 
+		a+=b;
+		cout <<' ';
+	}
+	cout << endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
