6a9a706f5102da3a15ad336031cb28431f397792
Towhid Zaman <towhid1zaman@gmail.com>
2020-09-06 18:55:00 +0600

    Add solution for problem `B - Vitamins` Link: https://codeforces.com/contest/1042/problem/B Tags: bitmasks, brute force, dp, implementation, *1200 Ref: https://codeforces.com/contest/1042/submission/92015846

---
+++ codeforces/1042/B.cpp
@@ -0,0 +1,102 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 1e8;// (int)3e18;
+const int maxn = 200005;
+
+/*ABC
+ *Only 7 types of combination , 2^3 - 1
+ */
+
+void task(){
+	int n; cin >> n;
+	vi Vitamins(8, inf);
+
+	rep(i, n){
+		int c; string s; cin >> c >> s;
+		int mask = 0;
+		for(int m = 0; m < s.size(); m++){
+			mask |= (1 << (s[m]-'A'));
+		}
+		Vitamins[mask] = min(Vitamins[mask], c);
+	}
+	int ans = inf;
+	for(int i = 0; i < 8; i++){
+		if(__builtin_popcount(i)==3){
+			ans = min(ans, Vitamins[i]);
+		}
+		for(int j = 0; j < 8; j++){
+			if(__builtin_popcount(i|j)==3){
+				ans = min(ans, Vitamins[i]+Vitamins[j]);
+			}
+			for(int k = 0; k < 8; k++){
+				if(__builtin_popcount(i|j|k)==3){
+					ans = min(ans, Vitamins[i] + Vitamins[j] + Vitamins[k]);
+				}
+			}
+		}
+	}
+
+	if(ans >= inf) ans = -1 ;
+	cout << ans << endl;
+
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
