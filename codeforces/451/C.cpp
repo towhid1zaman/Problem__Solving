bb299b45102e3e5c9c61aed5f54e4f932899f50f
Towhid Zaman <towhid1zaman@gmail.com>
2020-09-07 15:44:00 +0600

    Add solution for problem `C - Predict Outcome of the Game` Link: https://codeforces.com/contest/451/problem/C Tags: brute force, implementation, math, *1700 Ref: https://codeforces.com/contest/451/submission/92122107

---
+++ codeforces/451/C.cpp
@@ -0,0 +1,86 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("yes");else puts("no");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+ *
+ */
+
+bool chek(ll n, ll k, ll d1, ll d2){
+	bool check = n-k>=d1 and (n-k-d1)%3==0 and k>=d2 and (k-d2)%3==0;
+	return check;
+}
+
+void task(){
+	ll n, k, d1, d2; cin >> n >> k >> d1 >> d2;
+	bool ans = false;
+	
+	if(!ans) ans = chek(n, k, d1+d2*2, d1*2+d2);
+	if(!ans) ans = chek(n, k, d1*2+d2, d1+d2*2);
+	if(!ans) ans = chek(n, k, d1+d2, max(d1,d2)*2 - min(d1,d2));
+	if(!ans) ans = chek(n, k, max(d1,d2)*2 - min(d1, d2), d1+d2);
+
+	yn(ans);
+	
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
