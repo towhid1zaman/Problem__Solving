4be0aa5074dffe9c68e65f63ec960250a69c99df
Towhid Zaman <towhid1zaman@gmail.com>
2020-09-10 01:15:00 +0600

    Add solution for problem `B - Xenia and Spies` Link: https://codeforces.com/contest/342/problem/B Tags: brute force, greedy, implementation, *1500 Ref: https://codeforces.com/contest/342/submission/92381554

---
+++ codeforces/342/B.cpp
@@ -0,0 +1,100 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+ *
+ */
+
+void task(){
+	int n, m, s , f;
+	cin >> n >> m >> s >> f;
+	std::map<int, int>L,R;
+
+	for(int i = 1; i<=m; i++){
+		int t, l, r;
+		cin >> t >> l >> r;
+		L[t] = l;
+		R[t] = r;
+	}
+
+	string ans;
+
+	int pos = 1;
+	while(true){
+		if(s == f)break;
+		if(s < f){
+			if(s+1 >= L[pos] and s<=R[pos])ans+="X";
+			else ans+="R", s++;
+		}
+		else{
+			if(s >= L[pos] and s-1 <= R[pos])ans+="X";
+			else ans+="L", s--;
+		}
+
+		pos++;
+	}
+
+	cout << ans << endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
