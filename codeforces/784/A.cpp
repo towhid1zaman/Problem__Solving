19995ddb10ca200995b30e6c4af6b6358f9f6f87
Towhid Zaman <towhid1zaman@gmail.com>
2020-09-11 00:33:00 +0600

    Add solution for problem `A - Numbers Joke` Link: https://codeforces.com/contest/784/problem/A Tags: *special problem, *1700 Ref: https://codeforces.com/contest/784/submission/92452944

---
+++ codeforces/784/A.cpp
@@ -0,0 +1,74 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+ *
+ */
+
+void task(){
+        vi a = {4, 22, 27, 58, 85, 94, 121, 166, 202, 265, 274, 319, 346, 355, 378, 382, 391, 438, 454, 483, 517, 526, 535, 562, 576, 588, 627, 634, 636, 645, 648, 654, 663, 666, 690, 706, 728, 729, 762, 778, 825, 852, 861, 895, 913, 915, 922, 958, 985, 1086, 1111, 1165};
+        int n; cin >> n;
+        cout << a[n-1]<<endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1; //cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
