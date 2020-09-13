db545c428bee4aaf5304444876a064fa6b7dc40d
Towhid Zaman <towhid1zaman@gmail.com>
2020-09-13 21:12:00 +0600

    Add solution for problem `C - Chocolate Bunny` Link: https://codeforces.com/contest/1407/problem/C Tags: constructive algorithms, interactive, math, two pointers, *1600 Ref: https://codeforces.com/contest/1407/submission/92725975

---
+++ codeforces/1407/C.cpp
@@ -0,0 +1,92 @@
+
+// </> : towhid1zaman
+
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+//#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+
+/*
+ *
+ */
+int n;
+int a[maxn];
+
+int  Query(int x, int y){
+	cout << "? " << x <<' '<< y << endl;
+	cin >> x;
+	return x;
+}
+void task(){
+    cin >> n;
+    int p = 1;
+    for(int i = 2; i<=n; i++){
+    	int u = Query(i, p);
+    	int v = Query(p, i);
+    	if(u<v)a[p] = v, p = i;
+    	else a[i] = u;
+    }
+    a[p] = n;
+    cout <<"! ";
+    for(int i = 1;i<=n; i++){
+    	cout << a[i]<<' ';
+    }
+    cout <<endl;
+}
+
+int main(){
+        _
+        #ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+        #endif
+
+        int T = 1;// cin >> T;
+        while(T--){
+            task();
+        }
+        
+        
+return 0;
+}
+
+
