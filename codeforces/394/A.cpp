c3e1c278aee548050393aff9fcdd0b211b3f8b90
Towhid Zaman <towhid1zaman@gmail.com>
2020-11-09 20:36:00 +0600

    Add solution for problem `A - Counting Sticks` Link: https://codeforces.com/contest/394/problem/A Tags: brute force, implementation Ref: https://codeforces.com/contest/394/submission/97999365

---
+++ codeforces/394/A.cpp
@@ -0,0 +1,119 @@
+
+
+// </> : towhid1zaman
+
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+//#pragma GCC target("avx,avx2,fma") 
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+vi mods = {1000000007, 1000000009, 998244353};
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+const int mod = mods[0];
+
+/*
+ *
+ */
+
+void task(){
+        string s; cin >> s;
+        int befp = 0, afb = 0, afe = 0;
+        int n = sz(s);
+        int i = 0;
+        for(; i<n; i++){
+          if(s[i] == '+')break;
+          if(s[i] == '|')befp++;
+        }
+        i++;
+        for(; i<n; i++){
+          if(s[i] == '=')break;
+          if(s[i] == '|')afb++;
+        }
+        i++;
+        for(; i<n; i++){
+          if(s[i]=='|')afe++;
+        }
+        if(befp+afb == afe){
+          cout << s<< endl;
+          return;
+        }
+        if(befp+afb==afe+2){
+          if(befp>1)befp--;
+          else afb--;
+          afe++;
+        }
+        else if(befp+afb==afe-2){
+          befp++, afe--;
+        }
+        else{
+          cout <<"Impossible"<<endl;
+          return ;
+        }
+        for(int i = 0; i<befp; i++)cout <<'|';
+        cout <<'+';
+        for(int i = 0; i<afb; i++)cout <<'|';
+        cout <<"=";
+        for(int i = 0; i<afe; i++)cout <<'|';
+        cout << endl;
+       
+}
+
+int main(){
+        _
+#ifdef OJ
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+#endif
+
+        int T = 1; //cin >> T;
+        for(int __ = 1; __ <= T; __++){
+            //cout <<"Case "<<__<<": ";
+            task();
+        }
+
+
+/*array size ?
+ *n==1 ?
+ *different approach ?
+*/        
+return 0;
+}
+
+
+
+
+
