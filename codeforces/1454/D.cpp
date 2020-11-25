79687283e826a31b4ad11502795852b9bb6f728d
Towhid Zaman <towhid1zaman@gmail.com>
2020-11-25 19:22:00 +0600

    Add solution for problem `D - Number into Sequence` Link: https://codeforces.com/contest/1454/problem/D Tags: constructive algorithms, math, number theory, *1300 Ref: https://codeforces.com/contest/1454/submission/99563266

---
+++ codeforces/1454/D.cpp
@@ -0,0 +1,111 @@
+
+
+// </> : towhid1zaman
+
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+//#pragma GCC target("avx,avx2,fma") 
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+vi mods = {1000000007, 1000000009, 998244353};
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+const int mod = mods[0];
+
+/*
+ *
+ */
+
+
+  
+//prime factorization
+vector<pair<ll,ll>>factorize(ll a){
+    vector<pair<ll,ll> > pf;
+    for (ll i = 2; i * i <= a; ++i) {
+        if (a % i) continue;
+        pf.emplace_back(i, 0);
+        while (a % i == 0) {
+            ++pf.back().second;
+            a /= i;
+        }
+    }
+    if (a > 1) {
+        pf.emplace_back(a, 1);
+    }
+    return pf;
+}
+
+void task(){
+        ll n; cin >> n;
+        std::vector<pll> pfact = factorize(n);
+        ll most = 0, p = -1;
+
+        for(auto &a : pfact){
+            if(a.ss > most){
+                most = a.ss;
+                p = a.ff;
+            }
+        }
+
+        cout << most << endl;
+        std::vector<ll>ans(most, p);
+
+        while(n%p == 0) n/=p;
+        ans.back()*=n;
+
+        for(auto x:ans)cout << x << ' '; cout << endl;
+}
+
+int main(){
+        _
+#ifdef OJ
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+#endif
+
+        int T = 1; cin >> T;
+        for(int __ = 1; __ <= T; __++){
+            //cout <<"Case "<<__<<": ";
+            task();
+        }
+
+
+/*array size ?
+ *n==1 ?
+ *different approach ?
+*/        
+return 0;
+}
