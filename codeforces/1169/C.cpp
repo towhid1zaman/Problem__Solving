72e2fefefaf49091be43fd0197e68f28cafece3b
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-07 20:41:00 +0600

    Add solution for problem `C - Increasing by Modulo` Link: https://codeforces.com/contest/1169/problem/C Tags: binary search, greedy, *1700 Ref: https://codeforces.com/contest/1169/submission/100636417

---
+++ codeforces/1169/C.cpp
@@ -0,0 +1,106 @@
+
+
+// </> : towhid1zaman
+
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+//#pragma GCC target("avx,avx2,fma") 
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+vi mods = {1000000007, 1000000009, 998244353};
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+const int mod = mods[0];
+
+/*
+ *
+ */
+
+int n,m;
+std::vector<int>a;
+
+bool chek(int k){
+    int aa = 0;
+    for(int i = 0; i<n; i++){
+        int aaa = a[i];
+        int change = (aa - aaa + m)%m;
+        if(change <= k) continue;
+        if(aaa < aa) return 0;
+        aa = aaa;
+    }
+
+    return 1;
+}
+void task(){
+        cin >> n >> m;
+        a.resize(n);
+
+        for(int i = 0; i<n; i++){
+            cin >> a[i];
+        }
+
+        int upper = m-1, lower = 0;
+        while(lower < upper ){
+            int k = (upper + lower) / 2;
+            if(chek(k)){
+                upper = k;
+            }
+            else lower = k + 1;
+        }
+
+        cout << lower << endl;
+}
+
+int main(){
+        _
+#ifdef OJ
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+#endif
+
+        int T = 1; //cin >> T;
+        for(int __ = 1; __ <= T; __++){
+            //cout <<"Case "<<__<<": ";
+            task();
+        }
+
+
+/*array size ?
+ *n==1 ?
+ *different approach ?
+*/        
+return 0;
+}
