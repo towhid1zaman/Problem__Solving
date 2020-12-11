46f707ce9df191a7ce2db39e3887f93b584d42d5
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-11 22:53:00 +0600

    Add solution for problem `C - Fadi and LCM` Link: https://codeforces.com/contest/1285/problem/C Tags: brute force, math, number theory, *1400 Ref: https://codeforces.com/contest/1285/submission/100958595

---
+++ codeforces/1285/C.cpp
@@ -0,0 +1,102 @@
+
+
+// </> : towhid1zaman
+
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+//#pragma GCC target("avx,avx2,fma") 
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define deb(x) cout<<#x <<"="<<x<<endl;
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+vi mods = {1000000007, 1000000009, 998244353};
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+const int mod = mods[0];
+
+/*
+ *
+ */
+std::vector<ll>fact(ll n){
+    std::vector<ll> v;
+    for(ll i = 1; i*i<=n;i++){
+        if(n%i==0){
+            v.push_back(i);
+            if(i!=n/i){
+                v.push_back(n/i);
+            }
+        }
+    }
+    return v;
+}
+ll gcd(ll a,ll b){
+    return b ? gcd(b,a%b) : a;
+}
+ll lcm(ll a, ll b){
+    return a/ gcd(a,b)*b;
+}
+void task(){
+        ll n; cin >> n;
+        std::vector<ll>foct = fact(n);
+        sort(all(foct));
+        ll a;
+        for(ll i = 1; i*i<=n; i++){
+            if(n%i==0 and lcm(i, n/i) == n){
+                a = i;
+            }
+        }
+        cout << a <<' ';
+        cout << n/a<<endl;
+}
+
+int main(){
+        _
+#ifdef OJ
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+#endif
+
+        int T = 1; //cin >> T;
+        for(int __ = 1; __ <= T; __++){
+            //cout <<"Case "<<__<<": ";
+            task();
+        }
+
+
+/*array size ?
+ *n==1 ?
+ *different approach ?
+*/        
+return 0;
+}
