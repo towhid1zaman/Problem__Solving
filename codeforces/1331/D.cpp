22755f8cb2d0ee7328aa879adcd7c9590be649b2
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-15 20:04:00 +0600

    Add solution for problem `D - Again?` Link: https://codeforces.com/contest/1331/problem/D Tags: implementation Ref: https://codeforces.com/contest/1331/submission/101256464

--- codeforces/1331/D.cpp
+++ codeforces/1331/D.cpp
@@ -11,12 +11,12 @@ void task(){
         string s; cin >> s;
         int ans = 1;
         if((s[s.size()-1] - '0') % 2 == 0) ans = 0;
-        cout << ans << endl;        
+        cout << ans << endl;
 }
 
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+		ios::sync_with_stdio(0);
+		cin.tie(0);
 #ifdef OJ
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
