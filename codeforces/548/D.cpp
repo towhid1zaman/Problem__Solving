26d217d44388a6ca45d4782f0221ba6000cf34e4
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-15 20:44:00 +0600

    Add solution for problem `D - Mike and Feet` Link: https://codeforces.com/contest/548/problem/D Tags: binary search, data structures, dp, *1900 Ref: https://codeforces.com/contest/548/submission/101267710

--- codeforces/548/D.cpp
+++ codeforces/548/D.cpp
@@ -26,57 +26,58 @@ struct disjoint{
     	}
 }dsu;
 void task(){
-    int n; cin >> n;
-    dsu.Build(n);
-    std::vector<pair<int,int>>a(n);
-    std::vector<int>ans(n), visit(maxn,0);
-    for(int i = 0; i<n; i++){
-    	cin >> a[i].first;
-    	a[i].second = i;
-    }
+	    int n; cin >> n;
+	    dsu.Build(n);
+	    std::vector<pair<int,int>>a(n);
+	    std::vector<int>ans(n), visit(maxn,0);
+	    for(int i = 0; i<n; i++){
+	    	cin >> a[i].first;
+	    	a[i].second = i;
+	    }
 
-    sort(a.begin(), a.end());
-    reverse(a.begin(), a.end());
-    int l = 0;
-    for(int i = 0;i<n; i++){
-    	int A = a[i].second;
-    	int B = a[i].second - 1;
-    	if(A and visit[B]){
-    		dsu.Union(A, B);
-    	}
-    	B = a[i].second + 1;
-    	if(A < n-1 and visit[B]){
-    		dsu.Union(A, B);
-    	}
+	    sort(a.begin(), a.end());
+	    reverse(a.begin(), a.end());
+	    int l = 0;
+	    for(int i = 0;i<n; i++){
+	    	int A = a[i].second;
+	    	int B = a[i].second - 1;
+	    	if(A and visit[B]){
+	    		dsu.Union(A, B);
+	    	}
+	    	B = a[i].second + 1;
+	    	if(A < n-1 and visit[B]){
+	    		dsu.Union(A, B);
+	    	}
 
-    	visit[A] = 1;
-    	int C = dsu.Find(A);
-    	int Sz = dsu.Size(C);
-    	while(l < Sz){
-    		ans[l] = a[i].first;
-    		l++;
-    	}
-    }
+	    	visit[A] = 1;
+	    	int C = dsu.Find(A);
+	    	int Sz = dsu.Size(C);
+	    	while(l < Sz){
+	    		ans[l] = a[i].first;
+	    		l++;
+	    	}
+	    }
 
-    for(int i = 0; i<n; i++){
-    	cout << ans[i] <<' ';
-    }
-    cout << endl;
+	    for(int i = 0; i<n; i++){
+	    	cout << ans[i] <<' ';
+	    }
+	    cout << endl;
+
 }
 
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+		ios::sync_with_stdio(0);
+		cin.tie(0);
 #ifdef OJ
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
 #endif
 
-	int T = 1; //cin >> T;
-	for(int __ = 1; __ <= T; __++){
-	    //cout <<"Case "<<__<<": ";
-	    task();
-	}
+		int T = 1; //cin >> T;
+		for(int __ = 1; __ <= T; __++){
+		    //cout <<"Case "<<__<<": ";
+		    task();
+		}
       
 return 0;
 }
