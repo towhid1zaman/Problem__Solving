db47dd2dad9de10687f13141f0d47a797ab57213
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-17 15:30:00 +0600

    Add solution for problem `B2 - Books Exchange (hard version)` Link: https://codeforces.com/contest/1249/problem/B2 Tags: dfs and similar, dsu, math, *1300 Ref: https://codeforces.com/contest/1249/submission/101489727

---
+++ codeforces/1249/B2.cpp
@@ -0,0 +1,59 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+struct disjoint{
+        int fa[maxn];
+        int sz[maxn];
+        void Build(int n){
+            for(int i = 0; i<=n; i++) fa[i]=i, sz[i] = 1;
+        }
+        int Find(int x){ 
+            return x==fa[x]?x:fa[x]=Find(fa[x]);
+        }
+        void Union(int a, int b){ 
+            a = Find(a), b = Find(b);
+            if(a==b)return;
+            if(sz[a] < sz[b]) swap(a,b);
+            fa[b] = a;
+            sz[a] += sz[b];
+        }
+        int Size(int n){
+            return sz[n];
+        }
+}dsu;
+
+void task(){
+    int n; cin >> n;
+    dsu.Build(n);
+    for(int i = 1; i <= n;i++){
+        int p; cin >> p;
+        dsu.Union(p,i);
+    }
+    for(int i = 1; i <= n; i++){
+        cout << dsu.Size(dsu.Find(i))<<' ';
+    }
+    cout << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
