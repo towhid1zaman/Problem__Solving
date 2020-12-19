1f51acdaf7650f699c288a7aa0af1805bcb034a8
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-19 23:10:00 +0600

    Add solution for problem `A - Grasshopper And the String` Link: https://codeforces.com/contest/733/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/733/submission/101790973

---
+++ codeforces/733/A.cpp
@@ -0,0 +1,41 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+bool vl(char c){
+    return ( c== 'A' or c=='E' or c=='I' or c=='O' or c=='U' or c=='Y');
+}
+void task(){
+    string s; cin >> s;
+    int ans = 1, cur = 1;
+    for(int i = 0; i<s.size();i++){
+        if(vl(s[i])){
+            ans = max(cur, ans); // longest consonent
+            cur = 0;
+        }
+        cur++;
+    }
+    cout << max(ans, cur) << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
