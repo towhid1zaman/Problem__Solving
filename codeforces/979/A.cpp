8ec16ad0736ac0f834d06697b4f6ef477813bf07
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-23 01:06:00 +0600

    Add solution for problem `A - Pizza, Pizza, Pizza!!!` Link: https://codeforces.com/contest/979/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/979/submission/102070842

---
+++ codeforces/979/A.cpp
@@ -0,0 +1,33 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+
+void task(){
+    ll n; cin >> n;
+    ll ans = (!n?0:n+1);
+    if(n%2)ans=(n+1)/2;
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
