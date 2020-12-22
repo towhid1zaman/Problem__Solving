1b3cecece32f16477b6a8db18eaeb9184409541e
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-22 12:31:00 +0600

    Add solution for problem `A - Balanced Rating Changes` Link: https://codeforces.com/contest/1237/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1237/submission/102017441

---
+++ codeforces/1237/A.cpp
@@ -0,0 +1,35 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+int cnt = 0;
+void task(){
+    int n; cin >> n;
+    if(n%2==0) cout << n/2 << endl;
+    else{
+        cout << int(floor(n/2.0) + cnt )<< endl;
+        cnt = !cnt;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
