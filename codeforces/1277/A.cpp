990ee78fc92e0014ceed6d19a75c4a5692a9db92
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-24 01:02:00 +0600

    Add solution for problem `A - Happy Birthday, Polycarp!` Link: https://codeforces.com/contest/1277/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/1277/submission/102146425

---
+++ codeforces/1277/A.cpp
@@ -0,0 +1,40 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+
+void task(){
+    ll n; cin >> n;
+    int ans = 0;
+    // digit by digit 1,11,111..2,22,222...
+    for(int i = 1; i<=9; i++){
+        ll a = i;
+        while(a <= n){
+            a = 10*a + i;
+            ans++;
+        }
+    }
+    cout << ans <<  endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
