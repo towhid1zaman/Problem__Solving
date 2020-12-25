e47322d669c1c4a326904de1e7c5092c5d37c1bd
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-25 23:58:00 +0600

    Add solution for problem `A - Heating` Link: https://codeforces.com/contest/1260/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1260/submission/102342385

---
+++ codeforces/1260/A.cpp
@@ -0,0 +1,39 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+
+void task(){
+    int c, sum; cin >> c >> sum;
+    if(sum<c){
+        cout << sum << endl;
+        return;
+    }
+    int d = sum/c, r = sum%c;
+    int x = (c - r)*(d*d);
+    ++d;
+    int y = r * (d*d);
+    cout << x+y << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
