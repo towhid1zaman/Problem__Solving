bf99e00b395deaae365fc4302c6b3affabb56f6e
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-26 23:51:00 +0600

    Add solution for problem `A - Cashier` Link: https://codeforces.com/contest/1059/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/1059/submission/102418665

---
+++ codeforces/1059/A.cpp
@@ -0,0 +1,38 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+
+void task(){
+    ll n, L, a; cin >> n >> L >> a;
+    ll ans = 0, prev = 0;
+    for(int i = 0; i<n; i++){
+        ll t, l; cin >> t >> l;
+        ans+=(t - prev)/a;
+        prev = t+l; //total time per customer
+    }
+    ans+=(L-prev)/a;
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
