2e7406a187cc55a0494e52d8cafb17125691fcaa
Towhid Zaman <towhid1zaman@gmail.com>
2020-12-29 18:33:00 +0600

    Add solution for problem `A - Odds and Ends` Link: https://codeforces.com/contest/849/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/849/submission/102687312

---
+++ codeforces/849/A.cpp
@@ -0,0 +1,36 @@
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+
+void task(){
+    int n; cin >> n;
+    std::vector<int>a(n);
+    for(int i = 0; i<n; i++){
+        cin >> a[i];
+    }
+    bool ans = n%2 == 0 or a.back()%2==0 or a[0]%2==0;
+    cout << (ans? "NO" : "YES") << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
