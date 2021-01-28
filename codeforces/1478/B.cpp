e92568b554763b6e174871182bdd8b976ff9c120
Towhid Zaman <towhid1zaman@gmail.com>
2021-01-28 22:44:00 +0600

    Add solution for problem `B - Nezzar and Lucky Number` Link: https://codeforces.com/contest/1478/problem/B Tags: brute force, dp, greedy, math, *1100 Ref: https://codeforces.com/contest/1478/submission/105765504

---
+++ codeforces/1478/B.cpp
@@ -0,0 +1,133 @@
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+//#pragma GCC target("avx,avx2,fma") 
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define present(v,x) ((v).find(x)!=(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define yn(k) if(k)puts("YES");else puts("NO");
+
+vi mods = {1000000007, 1000000009, 998244353};
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 200005;
+const int mod = mods[0];
+
+/*
+ *
+ */
+ll d;
+vector<int>a(200, 0);
+bool checkLucky(int N){
+    while(N){
+        if(N%10==d)return 1;
+        N/=10;
+    }
+    return 0;
+}
+void pre(int mx){
+    for(int i = 1; i<=mx; i++){
+        if(checkLucky(i))a[i] =1;
+        else a[i] = -1;
+    }
+}
+int go(int x){
+    if(a[x]!=-1)return a[x];
+    for(int i = 1; i<x; i++){
+        //bool ok = checkLucky(i) and go(x-i);
+        if(checkLucky(i) and go(x-i)){
+            return a[x] = 1;
+        }
+    }
+    return a[x] = 0;
+}
+void task(){
+        ll q; cin >> q >> d;
+        ll mx = d*10;
+        pre(mx);
+        for(int i = 0; i<q; i++){
+            int x; cin >> x;
+            if(x>=mx){
+                cout <<"YES"<<endl;
+            }
+            else{
+                if(go(x)){
+                    cout << "YES"<<endl;
+                }
+                else cout <<"NO"<<endl;
+            }
+        }
+}
+
+int main(){
+        _
+#ifdef OJ
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+#endif
+
+        int T = 1; cin >> T;
+        for(int __ = 1; __ <= T; __++){
+            //cout <<"Case "<<__<<": ";
+            task();
+        }
+
+
+/*array size ?
+ *n==1 ?
+ *different approach ?
+*/        
+return 0;
+}
