fdb4ef8495e728dc25d4d624f5e6deecc619be94
Towhid Zaman <towhid1zaman@gmail.com>
2021-02-03 20:11:00 +0600

    Add solution for problem `K - Indivisibility` Link: https://codeforces.com/contest/630/problem/K Tags: math, number theory, *1500 Ref: https://codeforces.com/contest/630/submission/106376800

--- codeforces/630/K.cpp
+++ codeforces/630/K.cpp
@@ -7,42 +7,31 @@ typedef long long ll;
 const int maxn = 200005;
 const int mod = 1000000007;
 
-ll one(ll n){
-    ll cnt = 0;
-    while(n){
-        if(n%2)cnt++;
-        n/=2;
-    }
+ll inex(ll n, ll m){
+    return n/m;
 }
-bool ithBitSet(int mask, int pos){
-    //suppose mask = 10101, we have to check 3rd bit set or not
-    // then simply, 1<<3 = 100 = 8;
-    // then do AND operation , we know 1&1 = 1 only, rest are 0
-    return mask&(1<<pos);
-}
-
 void task(){
     ll n; cin >> n;
-    std::vector<int>d = {2, 3, 5, 7};
+    ll ans = 0;
     ll possible = n;
 
-    ll Numerator = n, Denominator; // a/b, a = Nume, b = Deno
-    
-    for(ll mask = 1; mask<=(1<<d.size()) - 1; mask++){
-        Denominator = 1ll;
-        std::vector<int>subset;
+    possible-=inex(n, 2);
+    possible-=inex(n, 3);
+    possible-=inex(n, 5);
+    possible-=inex(n, 7);
+    possible+=(inex(n, 2*3));
+    possible+=(inex(n, 2*5));
+    possible+=(inex(n, 2*7));
+    possible+=(inex(n, 3*5));
+    possible+=(inex(n, 3*7));
+    possible+=(inex(n, 5*7));
+    possible-=(inex(n, 2*3*5));
+    possible-=(inex(n, 2*3*7));
+    possible-=(inex(n, 2*5*7));
+    possible-=(inex(n, 3*5*7));
+    possible+=(inex(n, 2*3*5*7));
 
-        for(int i = 0; i<d.size(); i++){
-            if(ithBitSet(mask, i)){
-                subset.push_back(d[i]);
-            }
-        }
 
-        for(auto x:subset)Denominator*=x;   
-
-        if(subset.size()%2)possible-=(Numerator/Denominator);
-        else possible+=(Numerator/Denominator);
-    }
     cout << possible << endl;
 }
 
