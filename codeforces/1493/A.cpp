a1d322718103937969c7f718beb405daa0a09616
Towhid Zaman <towhid1zaman@gmail.com>
2021-03-07 13:07:00 +0600

    Add solution for problem `A - Anti-knapsack` Link: https://codeforces.com/contest/1493/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1493/submission/109313635

---
+++ codeforces/1493/A.cpp
@@ -0,0 +1,41 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    int n, k; cin >> n >> k;
+    std::vector<int>ans;
+    for(int i = n; i>k; i--)ans.push_back(i);
+    for(int i = k-1; i>=(k+1)/2; i--){
+        ans.push_back(i);
+    }
+    sort(all(ans));
+    cout << ans.size() << endl;
+    for(int i: ans)cout << i <<' ';
+    cout << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
