3129aea1f9ae3fb0501f64231a620fff4e55cb9d
Towhid Zaman <towhid1zaman@gmail.com>
2021-03-08 14:09:00 +0600

    Add solution for problem `D - MEX maximizing` Link: https://codeforces.com/contest/1294/problem/D Tags: data structures, greedy, implementation, math, *1600 Ref: https://codeforces.com/contest/1294/submission/109381044

---
+++ codeforces/1294/D.cpp
@@ -0,0 +1,40 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 400005;
+const int mod = 1000000007;
+
+void task(){
+    int q, x; cin >> q >> x;
+    std::vector<int>cnt(maxn, 0);
+    int mex = 0;
+    while(q--){
+        int a; cin >> a;
+        cnt[a%x]++;
+        while(cnt[mex%x] > 0) cnt[mex%x]--, mex++;
+        cout << mex << endl;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
