12eb245f53effbb34f9e21dce8043655174d63d4
Towhid Zaman <towhid1zaman@gmail.com>
2021-03-11 01:50:00 +0600

    Add solution for problem `A - Split it!` Link: https://codeforces.com/contest/1496/problem/A Tags: brute force, constructive algorithms, greedy, strings, *900 Ref: https://codeforces.com/contest/1496/submission/109655463

---
+++ codeforces/1496/A.cpp
@@ -0,0 +1,39 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+    string ans = "yes";
+    for(int i = 0; i<k; i++){
+        if(s[i] !=s[n-i-1])ans = "no";
+    }
+    if(n==2*k)ans="no";
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
