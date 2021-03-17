b3397e8bd50434aad9b7db8a26c9a4a210408531
Towhid Zaman <towhid1zaman@gmail.com>
2021-03-18 00:04:00 +0600

    Add solution for problem `A - Meximization` Link: https://codeforces.com/contest/1497/problem/A Tags: brute force, data structures, greedy, sortings, *800 Ref: https://codeforces.com/contest/1497/submission/110249427

---
+++ codeforces/1497/A.cpp
@@ -0,0 +1,47 @@
+/*
+これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました
+*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+#define pb push_back
+#define rep(i,a) for(int i=0;i<a;i++)
+#define all(v) (v).begin(),(v).end()
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    int n; cin >> n;
+    std::map<int,int>cnt;
+    rep(i, n){
+        int a; cin >> a;
+        cnt[a]++;
+    }
+    rep(i, 101){
+        if(cnt[i]){
+            cout << i <<' ';
+            cnt[i]--;
+        }
+    }
+    rep(i, 101)rep(j, cnt[i])cout << i <<' ';
+    cout << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifdef OJ
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
