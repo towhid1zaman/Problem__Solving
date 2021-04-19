12a9ad9db484a964528b1f21604d6b44bd9071b7
Towhid Zaman <towhid1zaman@gmail.com>
2021-04-19 22:29:00 +0600

    Add solution for problem `C - Product 1 Modulo N` Link: https://codeforces.com/contest/1514/problem/C Tags: constructive algorithms, math, number theory Ref: https://codeforces.com/contest/1514/submission/113537354

---
+++ codeforces/1514/C.cpp
@@ -0,0 +1,67 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbug(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+
+
+const int maxn = 200005;
+const int mod = 1000000007;
+
+ll gcd(ll a,ll b){
+    return b ? gcd(b,a%b) : a;
+}
+ll lcm(ll a, ll b){
+    return a/ gcd(a,b)*b;
+}
+
+void task(){
+    ll n; cin >> n;
+    vll a;
+
+    ll p = 1;
+    rep(i, n-2){
+        if(gcd(n, i+1)==1){
+            p*=i+1;
+            p%=n;
+            a.push_back(i+1);
+        }
+    }
+    if(p==n-1)a.push_back(n-1);
+    cout << a.size() << endl;
+    rep(i, a.size())cout << a[i] <<' ';cout << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
