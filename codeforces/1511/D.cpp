f3cbec348c74aadbf9cbb7bd1676746b62301149
Towhid Zaman <towhid1zaman@gmail.com>
2021-04-25 14:45:00 +0600

    Add solution for problem `D - Min Cost String` Link: https://codeforces.com/contest/1511/problem/D Tags: brute force, constructive algorithms, graphs, greedy, strings, *1600 Ref: https://codeforces.com/contest/1511/submission/114185245

---
+++ codeforces/1511/D.cpp
@@ -0,0 +1,50 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    int n, k; cin >> n >> k;
+    string ans;
+    rep(i, k){
+        ans.push_back(i+'a');
+        rep1(j, i+1, k-1){
+            ans.push_back(i+'a');
+            ans.push_back(j+'a');
+        }
+    }
+    int an = ans.size();
+    rep(i, n){
+        cout << ans[i%an];
+    }
+    cout << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
