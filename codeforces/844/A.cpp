ba3b759cb856a3f5b6eea8ca9cf02b8337767ad3
Towhid Zaman <towhid1zaman@gmail.com>
2021-06-01 19:46:00 +0600

    Add solution for problem `A - Diversity` Link: https://codeforces.com/contest/844/problem/A Tags: greedy, implementation, strings, *1000 Ref: https://codeforces.com/contest/844/submission/118096268

--- codeforces/844/A.cpp
+++ codeforces/844/A.cpp
@@ -15,6 +15,18 @@ typedef std::vector<ll> vll;
 const double pi = acos(-1.0);
 const int maxn = 200005;
 const int mod = 1000000007;
+namespace debugging{
+    template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+    template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+    template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+    template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+    template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+    #define dbug(args...) do {cerr << #args << " : "; Hooo(args); } while(0)
+    void Hooo(){cerr << endl;}
+    template<typename T>void Hooo(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+    template<typename T,typename...hello>void Hooo(T arg,const hello&...rest){cerr<<arg<<' ';Hooo(rest...);}
+}using namespace debugging;
+
 
 void task(){
     string s; cin >> s;
@@ -32,7 +44,9 @@ void task(){
         tot += c.second;
         ans += max(0, c.second-1);
     }
-    cout << max(0, k - (int)mark.size()) << endl;
+    //dbug(mark.size());
+    if(mark.size() > k)cout << 0 << endl;
+    else cout << k - mark.size()<<endl; 
 }
 
 int main(){
