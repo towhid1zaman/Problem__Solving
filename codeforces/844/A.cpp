ddd3ae24ed889ff3ad58472c8c032c125212513b
Towhid Zaman <towhid1zaman@gmail.com>
2021-06-01 19:51:00 +0600

    Add solution for problem `A - Diversity` Link: https://codeforces.com/contest/844/problem/A Tags: greedy, implementation, strings, *1000 Ref: https://codeforces.com/contest/844/submission/118096828

---
+++ codeforces/844/A.cpp
@@ -0,0 +1,52 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    string s; cin >> s;
+    int k; cin >> k;
+    int n = s.size();
+    if(k > n){
+        cout <<"impossible" << endl;
+        return;
+    }
+
+    std::map<char, int> mark;
+    for(char c: s)mark[c]++;
+    int ans = 0, tot = 0;
+    for(auto c: mark){
+        tot += c.second;
+        ans += max(0, c.second-1);
+    }
+    cout << max(0, k - (int)mark.size()) << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
