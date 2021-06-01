c13c12fa0ce6ec56e507b2d650e17e6ad18b15eb
Towhid Zaman <towhid1zaman@gmail.com>
2021-06-01 10:07:00 +0600

    Add solution for problem `A - Shell Game` Link: https://codeforces.com/contest/777/problem/A Tags: constructive algorithms, implementation, math, *1000 Ref: https://codeforces.com/contest/777/submission/118032449

---
+++ codeforces/777/A.cpp
@@ -0,0 +1,43 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 200005;
+const int mod = 1000000007;
+
+void task(){
+    ll n; cin >> n;
+    int x; cin >> x;
+    if(n%2==1){
+        if(x==0)x=1;
+        else if(x==1)x=0;
+    }
+    cout << (x+(n/2))%3<<endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; //cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
