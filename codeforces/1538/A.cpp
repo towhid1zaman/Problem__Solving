ff03395f94846dca2788005a8ad08e226502d2a8
Towhid Zaman <towhid1zaman@gmail.com>
2021-06-10 23:46:00 +0600

    Add solution for problem `A - Stone Game` Link: https://codeforces.com/contest/1538/problem/A Tags: greedy Ref: https://codeforces.com/contest/1538/submission/119084134

---
+++ codeforces/1538/A.cpp
@@ -0,0 +1,46 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 2000006;
+const int mod = 1000000007;
+
+void task(){
+    int n; cin >> n;
+    int mn = 0, mx = 0;
+    for(int i = 0; i<n; i++){
+        int a; cin >> a;
+        if(a==1)mn=i+1;
+        if(a==n)mx=i+1;
+    }
+    if(mn>mx)swap(mn, mx);
+    int t1 = mn + (n - mx + 1), t2 = max((n-mx+1), (n-mn+1));
+    cout << min({mx, t1, t2}) << endl;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
