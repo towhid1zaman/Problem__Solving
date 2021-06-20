c2e6f6463f9918c54d05f416f078df8d1a9730d4
Towhid Zaman <towhid1zaman@gmail.com>
2021-06-20 16:37:00 +0600

    Add solution for problem `A - Contest Start` Link: https://codeforces.com/contest/1539/problem/A Tags: combinatorics, math Ref: https://codeforces.com/contest/1539/submission/120076691

---
+++ codeforces/1539/A.cpp
@@ -0,0 +1,44 @@
+/*これを翻訳している間、あなたはあなたの人生のいくつかの貴重な瞬間を無駄にしました*/
+#pragma comment (linker,"/STACK:16777216")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vll;
+#define endl "\n"
+#define pb push_back
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define all(v) (v).begin(),(v).end()
+
+const double pi = acos(-1.0);
+const int maxn = 2000006;
+const int mod = 1000000007;
+
+void task(){
+    ll a, b, c; cin >> a >> b >> c;
+    ll cur = min(a-1, (ll)c/b);
+    if(cur == 0){
+        cout << 0 << endl; return;
+    }
+    else{
+        cout << ( ( max(0ll, cur*(cur-1)/2) ) + cur*(a-cur) )<< endl;
+    }
+}
+
+int main(){
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifdef _Local
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int T = 1; cin >> T;
+    for(int __ = 1; __ <= T; __++){
+        //cout <<"Case "<<__<<": ";
+        task();
+    }
+      
+return 0;
+}
