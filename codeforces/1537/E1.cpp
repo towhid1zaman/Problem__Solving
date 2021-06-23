0edba898d010dcda948bf1777bed92ea5083af07
Towhid Zaman <towhid1zaman@gmail.com>
2021-06-23 12:24:00 +0600

    Add solution for problem `E1 - Erase and Extend (Easy Version)` Link: https://codeforces.com/contest/1537/problem/E1 Tags: binary search, brute force, dp, greedy, hashing, implementation, string suffix structures, strings, two pointers, *1600 Ref: https://codeforces.com/contest/1537/submission/120341946

--- codeforces/1537/E1.cpp
+++ codeforces/1537/E1.cpp
@@ -16,101 +16,21 @@ const double pi = acos(-1.0);
 const int maxn = 2000006;
 const int mod = 1000000007;
 
-
-namespace String_Matching{
-    /*Z-Algo, build an array size of string s
-     *In each element of array it store an element that says
-     *exactly 'x' size of substring can be found from this position 
-     *that is equal to Prefix of given string.
-     */
-    std::vector<int> z_function(string s){
-        int n = (int) s.length();
-        std::vector<int> z(n, 0);
-        //z[0] = n; //whole string is a substring of itself.
-        for (int i = 1, l = 0, r = 0; i < n; ++i){
-            if (i <= r)
-                z[i] = min (r - i + 1, z[i - l]);
-            while (i + z[i] < n && s[z[i]] == s[i + z[i]])
-                ++z[i];
-            if (i + z[i] - 1 > r)
-                l = i, r = i + z[i] - 1;
-        }
-        return z;
-    }
-
-    /*Given a string S (with length N) and a pattern P (with length M),
-     *KMP, finds all the occurrence of P in S
-     */
-    std::vector<int> prefix_function(string s){
-        int n = (int)s.length();
-        std::vector<int> pi(n);
-        for (int i = 1; i < n; i++) {
-            int j = pi[i-1];
-            while (j > 0 && s[i] != s[j])
-                j = pi[j-1];
-            if (s[i] == s[j])
-                j++;
-            pi[i] = j;
-        }
-        return pi;
-    }
-
-    std::vector<int> KMP_match(string text, string pattern, std::vector<int>&pi){
-        int n = (int)text.length(), m = (int)pattern.length();
-        std::vector<int> Prefix = pi;
-        std::vector<int>Match_position;
-        int j = 0, i = 0;
-        // i = text, j = pattern
-        while (i < n) {
-            if (text[i] == pattern[j]) ++i, ++j;
-            else {
-                if (j != 0) j = Prefix[j - 1];
-                else ++i;
-            }
-            if (j == (int) pattern.length()) {
-                Match_position.push_back(i-m);
-                j = Prefix[j-1];
-            }
-        }
-        return Match_position;
-    }
-    /*Counting the number of occurrences of each prefix
-     *occur(given string, prefix value)
-     */
-    std::vector<int> occur(string s, std::vector<int>&pi){
-        int n = (int)s.length();
-        std::vector<int>ans(n+1, 0);
-        for (int i = 0; i < n; i++)ans[pi[i]]++;
-        for (int i = n-1; i > 0; i--)ans[pi[i-1]] += ans[i];
-        for (int i = 0; i <= n; i++)ans[i]++;
-
-        return ans;
-    }
-
-    //check range, 0 indexed
-}using namespace String_Matching;
-
 void task(){
     int n, k; cin >> n >> k;
     string s; cin >> s;
-    std::vector<int> z = z_function(s);
-
-    int p = 1;
-    while(p < n){
-        if(s[p] > s[0])break;
-        else if(s[p] < s[0]){
-            p++;
-            continue;
-        }
-
-        int common = z[p];
-        if(common + p >= n)break;
-        if(s[common] > s[p + common])p++;
-        else break;
+    int l = 0, i;
+    for(i = 1; i<n;i++){
+        if(s[i]==s[l])l++;
+        else if(s[i] > s[l]){break;}
+        else l = 0;
     }
-    for(int i = 0; i<k; i++){
-        cout << s[i%p];
+    i-=l;
+    for(int ii = 0; ii<k; ii++){
+        cout << s[ii%i];
     }
+    cout << endl;
+
 }
 
 int main(){
